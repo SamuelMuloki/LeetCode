f53786523ee99b3919dfded77c0eb2517563dbf0
Samuel Muloki <samuellebron0@gmail.com>
2020-05-18 19:44:48 +0300

    [Update] import and use package utils

--- hello-world.go
+++ hello-world.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/SamuelMuloki/GOExamples/utils"
+)
 
 func main() {
+	a, b := utils.Swap("hello", "world")
 	fmt.Println("hello world")
 	fmt.Println("Hello, 世界")
+	fmt.Println(a, b)
 }
