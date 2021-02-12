d0f87b5d33ee02d9bd486ba4dac5fea72e9cd0ee
Samuel Muloki <samuellebron0@gmail.com>
2021-02-12 22:34:04 +0300

    [Update] Rename hello-world to main

--- playground/mutex/mutex.go
+++ playground/mutex/mutex.go
@@ -20,7 +20,7 @@ func main() {
 	var m sync.Mutex
 	for i := 0; i < 1000; i++ {
 		w.Add(1)
-		// its imnportant to pass the addess of the mutex if the mutext is passed by value instead of
+		// its important to pass the addess of the mutex if the mutext is passed by value instead of
 		// passing the address, each go routine will have its own copy of the mutex and a race condition will occur
 		go increment(&w, &m)
 	}
