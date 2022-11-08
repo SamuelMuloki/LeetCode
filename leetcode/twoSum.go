f5257c30bf9c158570d7054fb47baf98c662a51a
Samuel Muloki <samuellebron0@gmail.com>
2022-11-08 21:00:32 +0300

    [Update] Improve two sum

--- leetcode/twoSum.go
+++ leetcode/twoSum.go
@@ -2,16 +2,14 @@ package leetcode
 
 func TwoSum(nums []int, target int) []int {
 	complements := make(map[int]int, len(nums))
-	indices := make([]int, 0, 2)
 
 	for i, val := range nums {
-
 		if comp, ok := complements[val]; ok {
-			indices = []int{comp, i}
+			return []int{comp, i}
 		}
 
 		complements[target-val] = i
 	}
 
-	return indices
+	return []int{}
 }
