430f3ec4034997ebdfec7e93363d6757c31be2af
Samuel Muloki <samuellebron0@gmail.com>
2022-11-18 11:22:23 +0300

    [Update] Another sulution for three sum

--- leetcode/threeSum.go
+++ leetcode/threeSum.go
@@ -1,6 +1,8 @@
 package leetcode
 
-import "sort"
+import (
+	"sort"
+)
 
 func ThreeSum(nums []int) [][]int {
 	var result [][]int
@@ -16,17 +18,21 @@ func ThreeSum(nums []int) [][]int {
 
 		for k, j := i+1, len(nums)-1; k < j; {
 			n := nums[i] + nums[k] + nums[j]
-			if n == 0 {
-				result = append(result, []int{nums[i], nums[k], nums[j]})
-				l := k
-				for l < j && nums[l] == nums[k] {
-					l++
-				}
-				k = l
-			} else if n > 0 {
+			if n > 0 {
 				j--
-			} else {
+			} else if n < 0 {
 				k++
+			} else {
+				result = append(result, []int{nums[i], nums[k], nums[j]})
+				for k < j && nums[k] == nums[k+1] {
+					k++
+				}
+
+				for k < j && nums[j] == nums[j-1] {
+					j--
+				}
+				k++
+				j--
 			}
 		}
 	}
