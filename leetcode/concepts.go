a1d299ebc23736c8353291c01f3ce864bf516106
Samuel Muloki <samuellebron0@gmail.com>
2022-11-24 22:35:30 +0300

    [Update] Check if valid parentheses

--- leetcode/concepts.go
+++ leetcode/concepts.go
@@ -19,3 +19,15 @@ func MaxSum(arr []int, k int) int {
 
 	return max_sum
 }
+
+// Reverse a string
+
+func Reverse(s string) string {
+	runes := []rune(s)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
