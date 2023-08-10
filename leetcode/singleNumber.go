bfa332a19246abd4d7ac26b964dad10a0d25b4a9
Samuel Muloki <samuellebron0@gmail.com>
2023-08-10 23:22:50 +0300

    [Update] Add single number xor solution

--- leetcode/singleNumber.go
+++ leetcode/singleNumber.go
@@ -1,18 +1,10 @@
 package leetcode
 
 func SingleNumber(nums []int) int {
-	hash := make(map[int]int)
-
-	var minVal int
-	for idx := range nums {
-		hash[nums[idx]]++
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		res ^= nums[i]
 	}
 
-	for i, k := range hash {
-		if k == 1 {
-			minVal = i
-		}
-	}
-
-	return minVal
+	return res
 }
