25c072f932870622d66e188504c4bdf4efa8d2c7
Samuel Muloki <samuellebron0@gmail.com>
2023-08-11 14:36:34 +0300

    [Update] Add another hamming weight solution

--- leetcode/hammingWeight.go
+++ leetcode/hammingWeight.go
@@ -1,7 +1,7 @@
 package leetcode
 
 func HammingWeight(num uint32) (count int) {
-	for ; num != 0; num, count = num&(num-1), count+1 {
+	for ; num != 0; num, count = num>>1, count+int(num&1) {
 	}
 	return
 }
