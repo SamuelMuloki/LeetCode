663bbc3b7ae96925fe4a9887ef6784b3368edfda
Samuel Muloki <samuellebron0@gmail.com>
2023-08-17 21:55:23 +0300

    [Update] Remove question comments

--- leetcode/topKFrequent.go
+++ leetcode/topKFrequent.go
@@ -4,17 +4,6 @@ import (
 	"sort"
 )
 
-/*
-Given an integer array nums and an integer k, return the
-k most frequent elements. You may return the answer in any order.
-
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
-
-Input: nums = [1], k = 1
-Output: [1]
-*/
-
 func TopKFrequent(nums []int, k int) []int {
 	frequency := make(map[int]int)
 
