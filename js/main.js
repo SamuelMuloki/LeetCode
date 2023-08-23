0d42a40bc740d75b4146d78bf0b0c75f3183ed45
Samuel Muloki <samuellebron0@gmail.com>
2023-08-23 15:52:20 +0300

    [Update] Add day 2 js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ var { containsDuplicate } = require("./0217-contains-duplicate");
 var { isAnagram } = require("./0242-valid-anagram");
 var { convertToTitle } = require("./0168-excel-sheet-column-title");
 var { createHelloWorld } = require("./2667-create-hello-world-function")
+var { createCounter } = require("./2620-counter")
 
 console.log(containsDuplicate([1, 2, 3, 1]));
 console.log(isAnagram("rat", "car"));
 console.log(convertToTitle(701));
 console.log(createHelloWorld()())
+const counter = createCounter(10)
+console.log(counter())
+console.log(counter())
+console.log(counter())
