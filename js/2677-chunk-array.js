e6094c8a48b20a498745d008f16624cb11216fa3
Samuel Muloki <samuellebron0@gmail.com>
2023-08-27 23:17:30 +0300

    [Update] Add day 21 short solution

--- js/2677-chunk-array.js
+++ js/2677-chunk-array.js
@@ -4,19 +4,9 @@
  * @return {Array[]}
  */
 var chunk = function (arr, size) {
-  let output = new Array(),
-    chunkArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    chunkArr.push(arr[i]);
-    if (chunkArr.length === size) {
-      output.push(chunkArr);
-      chunkArr = [];
-    }
-  }
-
-  if (chunkArr.length) {
-    output.push(chunkArr)
-  }
+  let output = [];
+  for (let i = 0, len = arr.length; i < len; )
+    output.push(arr.slice(i, (i += size)));
 
   return output;
 };
