a32c47f878f4cf78e95f9a97a60bfd9a6ed6f53f
Samuel Muloki <samuellebron0@gmail.com>
2023-09-05 15:12:05 +0300

    [Update] Add shortest add vowel solution

--- js/0345-reverse-vowels-of-a-string.js
+++ js/0345-reverse-vowels-of-a-string.js
@@ -3,45 +3,8 @@
  * @return {string}
  */
 var reverseVowels = function (s) {
-  var i = 0,
-    j = s.length - 1;
-
-  var isVowel = function (c) {
-    return (
-      c == 'a' ||
-      c == 'e' ||
-      c == 'i' ||
-      c == 'o' ||
-      c == 'u' ||
-      c == 'A' ||
-      c == 'E' ||
-      c == 'I' ||
-      c == 'O' ||
-      c == 'U'
-    );
-  };
-
-  s = s.split("");
-  while (i < j) {
-    if (!isVowel(s[i])) {
-      i++;
-      continue;
-    }
-
-    if (!isVowel(s[j])) {
-      j--;
-      continue;
-    }
-
-    var temp = s[i];
-    s[i] = s[j];
-    s[j] = temp;
-
-    i++;
-    j--;
-  }
-
-  return s.join("");
+  var vowel = s.match(/[aeiou]/gi);
+  return s.replace(/[aeiou]/gi, (_) => vowel.pop());
 };
 
 module.exports = { reverseVowels };
