46eb8ac6c52e0f2c423018c1dc250b6e280cdbc7
Samuel Muloki <samuellebron0@gmail.com>
2023-09-21 16:14:51 +0300

    [Update] Remove zero

--- go/solutions/0014-longest-common-prefix.go
+++ go/solutions/0014-longest-common-prefix.go
@@ -8,7 +8,7 @@ func LongestCommonPrefix(strs []string) string {
 	for i := 0; i < len(strs[0]); i++ {
 		for j := 1; j < len(strs); j++ {
 			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
-				return strs[0][0:i]
+				return strs[0][:i]
 			}
 		}
 	}
