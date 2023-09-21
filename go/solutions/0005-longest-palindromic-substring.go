c7dbf855ea35b5439fd7c90a9a448f2048f7a607
Samuel Muloki <samuellebron0@gmail.com>
2023-09-21 14:52:11 +0300

    [Update] Add faster longest substring

--- go/solutions/0005-longest-palindromic-substring.go
+++ go/solutions/0005-longest-palindromic-substring.go
@@ -2,17 +2,38 @@
 package solutions
 
 func LongestPalindrome(s string) string {
-	max, maxStr := 1, string(s[0])
-	for i := 0; i < len(s); i++ {
-		for j := i + 1; j < len(s); j++ {
-			str := string(s[i]) + string(s[i+1:j+1])
+	n := len(s)
+	dp := make([][]bool, n)
+	output := []int{0, 0}
 
-			if isPalindrome(str) && len(str) > max {
-				maxStr = str
-				max = len(str)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]bool, n)
+	}
+
+	for i := 0; i < n; i++ {
+		dp[i][i] = true
+	}
+
+	for i := 0; i < n-1; i++ {
+		if string(s[i]) == string(s[i+1]) {
+			dp[i][i+1] = true
+			output[0] = i
+			output[1] = i + 1
+		}
+	}
+
+	for diff := 2; diff < n; diff++ {
+		for i := 0; i < n-diff; i++ {
+			j := i + diff
+			if string(s[i]) == string(s[j]) && dp[i+1][j-1] {
+				dp[i][j] = true
+				output[0] = i
+				output[1] = j
 			}
 		}
 	}
 
-	return maxStr
+	i, j := output[0], output[1]
+
+	return string(s[i : j+1])
 }
