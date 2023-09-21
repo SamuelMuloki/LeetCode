84e2091b0957170e3952c94a551919755e9361e1
Samuel Muloki <samuellebron0@gmail.com>
2023-09-21 16:49:28 +0300

    [Update] Use binary search

--- go/solutions/0014-longest-common-prefix.go
+++ go/solutions/0014-longest-common-prefix.go
@@ -1,17 +1,40 @@
 package solutions
 
+import (
+	"math"
+	"strings"
+)
+
 func LongestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
 	}
 
-	for i := 0; i < len(strs[0]); i++ {
-		for j := 1; j < len(strs); j++ {
-			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
-				return strs[0][:i]
-			}
+	minLen := math.MaxInt
+	for i := range strs {
+		minLen = min(minLen, len(strs[i]))
+	}
+
+	l, r := 1, minLen
+	for l <= r {
+		mid := (l + r) / 2
+		if isCommonPrefix(strs, mid) {
+			l = mid + 1
+		} else {
+			r = mid - 1
 		}
 	}
 
-	return strs[0]
+	return strs[0][:(l+r)/2]
+}
+
+func isCommonPrefix(strs []string, mid int) bool {
+	str1 := strs[0][:mid]
+	for i := 1; i < len(strs); i++ {
+		if !strings.HasPrefix(strs[i], str1) {
+			return false
+		}
+	}
+
+	return true
 }
