6eb48cccd103a26c80bb138178dd9ccb21559093
Samuel Muloki <samuellebron0@gmail.com>
2023-10-05 11:26:16 +0300

    [Update] Use boyer algorithm

--- go/solutions/0169-majority-elemennt.go
+++ go/solutions/0169-majority-elemennt.go
@@ -1,22 +1,16 @@
 // https://leetcode.com/problems/majority-element/
 package solutions
 
-import (
-	"sort"
-)
-
 func MajorityElement(nums []int) int {
-	sort.Ints(nums)
 	res, count := nums[0], 1
 	for i := 1; i < len(nums); i++ {
-		if nums[i] == nums[i-1] {
+		if count == 0 {
+			count++
+			res = nums[i]
+		} else if res == nums[i] {
 			count++
 		} else {
-			count = 1
-		}
-
-		if (len(nums) / 2) < count {
-			return nums[i]
+			count--
 		}
 	}
 
