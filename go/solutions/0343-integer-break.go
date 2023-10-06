cdc3b97d8012c424c292c610d4bdfa59d31dff4e
Samuel Muloki <samuellebron0@gmail.com>
2023-10-06 13:39:40 +0300

    [Update] Add optimised integer break

--- go/solutions/0343-integer-break.go
+++ go/solutions/0343-integer-break.go
@@ -1,32 +1,22 @@
 // https://leetcode.com/problems/integer-break/
 package solutions
 
-import "github.com/SamuelMuloki/LeetCode/go/utils"
+import (
+	"math"
+)
 
 func IntegerBreak(n int) int {
 	if n <= 3 {
 		return n - 1
 	}
 
-	dp := make([]int, n+1)
-	var dfs func(num int) int
-	dfs = func(num int) int {
-		if num <= 3 {
-			return num
-		}
-
-		if dp[num] != 0 {
-			return dp[num]
-		}
-
-		res := num
-		for i := 2; i < num; i++ {
-			res = utils.Max(res, i*dfs(num-i))
-		}
-
-		dp[num] = res
-		return dp[num]
+	if n%3 == 0 {
+		return int(math.Pow(float64(3), float64(n/3)))
 	}
 
-	return dfs(n)
+	if n%3 == 1 {
+		return int(math.Pow(float64(3), float64((n/3)-1))) * 4
+	}
+
+	return int(math.Pow(float64(3), float64(n/3))) * 2
 }
