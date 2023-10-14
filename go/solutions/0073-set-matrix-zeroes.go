c2f651020578272e73b788672657ae0a62f074cf
Samuel Muloki <samuellebron0@gmail.com>
2023-10-14 16:49:20 +0300

    [Update] Add set zeroes space optimized

--- go/solutions/0073-set-matrix-zeroes.go
+++ go/solutions/0073-set-matrix-zeroes.go
@@ -1,25 +1,29 @@
 package solutions
 
 func SetZeroes(matrix [][]int) {
-	rc, cr := make(map[int]int), make(map[int]int)
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[i]); j++ {
+	col0, rows, cols := false, len(matrix), len(matrix[0])
+	for i := 0; i < rows; i++ {
+		if matrix[i][0] == 0 {
+			col0 = true
+		}
+
+		for j := 1; j < cols; j++ {
 			if matrix[i][j] == 0 {
-				cr[j] = i
-				rc[i] = j
+				matrix[0][j] = 0
+				matrix[i][0] = 0
 			}
 		}
 	}
 
-	for r := range rc {
-		for i := 0; i < len(matrix[r]); i++ {
-			matrix[r][i] = 0
+	for i := rows - 1; i >= 0; i-- {
+		for j := cols - 1; j >= 1; j-- {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
 		}
-	}
 
-	for c := range cr {
-		for i := 0; i < len(matrix); i++ {
-			matrix[i][c] = 0
+		if col0 {
+			matrix[i][0] = 0
 		}
 	}
 }
