a7ac0f88bd72866f12f3b916f5d1283e4dfabce4
Samuel Muloki <samuellebron0@gmail.com>
2023-10-15 14:30:46 +0300

    [Update] Add space optimized dp

--- go/solutions/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.go
+++ go/solutions/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.go
@@ -9,11 +9,8 @@ func NumberOfWays(startPos int, endPos int, k int) int {
 		Note that for d == 0, the number of ways is dfs(k - 1, 1) + dfs(k - 1, 1), since abs(0 - 1) == 1.
 		We can reach 0 from positions 1 and -1, and the number of ways for negative positions mirrors positive positions.
 	*/
-	dp := make([][]int, 1001)
-	for i := range dp {
-		dp[i] = make([]int, 1001)
-	}
-
+	dp := make([]int, 1001)
+	prevDp := make([]int, 1001)
 	var abs = func(diff int) int {
 		if diff < 0 {
 			return -diff
@@ -22,12 +19,14 @@ func NumberOfWays(startPos int, endPos int, k int) int {
 		return diff
 	}
 
-	for k := 1; k <= 1000; k++ {
-		dp[k][k] = 1
-		for i := 0; i < k; i++ {
-			dp[k][i] = (dp[k-1][i+1] + dp[k-1][abs(i-1)]) % (1e9 + 7)
+	for j := 1; j <= k; j++ {
+		dp = make([]int, 1001)
+		dp[j] = 1
+		for i := 0; i < j; i++ {
+			dp[i] = (prevDp[i+1] + prevDp[abs(i-1)]) % (1e9 + 7)
 		}
+		prevDp = dp
 	}
 
-	return dp[k][abs(startPos-endPos)]
+	return dp[abs(startPos-endPos)]
 }
