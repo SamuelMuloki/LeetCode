fd24761e36cbc3855d9ddd9307bc389b1ed87fa0
Samuel Muloki <samuellebron0@gmail.com>
2023-10-15 16:14:55 +0300

    [Update] Add bottom up approach

--- go/solutions/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.go
+++ go/solutions/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.go
@@ -1,6 +1,8 @@
 package solutions
 
-import "github.com/SamuelMuloki/LeetCode/go/utils"
+import (
+	"github.com/SamuelMuloki/LeetCode/go/utils"
+)
 
 func NumWays(steps int, arrLen int) int {
 	arrLen = utils.Min(arrLen, steps)
@@ -9,37 +11,21 @@ func NumWays(steps int, arrLen int) int {
 		dp[i] = make([]int, steps+1)
 	}
 
-	for i := range dp {
-		for k := range dp[i] {
-			dp[i][k] = -1
-		}
-	}
-
-	var dfs func(curr, remain int) int
-	dfs = func(curr, remain int) int {
-		if remain == 0 {
-			if curr == 0 {
-				return 1
+	dp[0][0] = 1
+	for remain := 1; remain <= steps; remain++ {
+		for curr := arrLen - 1; curr >= 0; curr-- {
+			res := dp[curr][remain-1]
+			if curr > 0 {
+				res = (res + dp[curr-1][remain-1]) % (1e9 + 7)
 			}
-			return 0
-		}
 
-		if dp[curr][remain] != -1 {
-			return dp[curr][remain]
-		}
+			if curr < arrLen-1 {
+				res = (res + dp[curr+1][remain-1]) % (1e9 + 7)
+			}
 
-		res := dfs(curr, remain-1)
-		if curr > 0 {
-			res = (res + dfs(curr-1, remain-1)) % (1e9 + 7)
+			dp[curr][remain] = res
 		}
-
-		if curr < arrLen-1 {
-			res = (res + dfs(curr+1, remain-1)) % (1e9 + 7)
-		}
-
-		dp[curr][remain] = res
-		return res
 	}
 
-	return dfs(0, steps)
+	return dp[0][steps]
 }
