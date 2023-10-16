2256b3cefb6834f0d135883da7347ced93146d01
Samuel Muloki <samuellebron0@gmail.com>
2023-10-16 19:37:20 +0300

    [Update] Add space optimised dp

--- go/solutions/0120-triangle.go
+++ go/solutions/0120-triangle.go
@@ -6,17 +6,12 @@ import (
 
 func MinimumTotal(triangle [][]int) int {
 	n := len(triangle)
-	dp := make([][]int, n)
-	for i := range dp {
-		dp[i] = make([]int, n)
-	}
-
-	dp[n-1] = triangle[n-1]
+	minPath := triangle[n-1]
 	for i := n - 2; i >= 0; i-- {
 		for j := 0; j <= i; j++ {
-			dp[i][j] = utils.Min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j]
+			minPath[j] = utils.Min(minPath[j], minPath[j+1]) + triangle[i][j]
 		}
 	}
 
-	return dp[0][0]
+	return minPath[0]
 }
