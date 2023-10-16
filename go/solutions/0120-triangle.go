80c3369289e9cd91eb0d6f3c2b4f67fad73fd01c
Samuel Muloki <samuellebron0@gmail.com>
2023-10-16 19:36:40 +0300

    [Update] Add bottom up

--- go/solutions/0120-triangle.go
+++ go/solutions/0120-triangle.go
@@ -1,36 +1,22 @@
 package solutions
 
-import "github.com/SamuelMuloki/LeetCode/go/utils"
+import (
+	"github.com/SamuelMuloki/LeetCode/go/utils"
+)
 
 func MinimumTotal(triangle [][]int) int {
 	n := len(triangle)
-	dp := make([][]int, n+1)
+	dp := make([][]int, n)
 	for i := range dp {
-		dp[i] = make([]int, n+1)
+		dp[i] = make([]int, n)
 	}
 
-	for i := range dp {
-		for j := range dp[i] {
-			dp[i][j] = -1
+	dp[n-1] = triangle[n-1]
+	for i := n - 2; i >= 0; i-- {
+		for j := 0; j <= i; j++ {
+			dp[i][j] = utils.Min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j]
 		}
 	}
 
-	var dfs func(i, j int) int
-	dfs = func(i, j int) int {
-		if i == n {
-			return 0
-		}
-
-		if dp[i][j] != -1 {
-			return dp[i][j]
-		}
-
-		lowerLeft := triangle[i][j] + dfs(i+1, j)
-		lowerRight := triangle[i][j] + dfs(i+1, j+1)
-
-		dp[i][j] = utils.Min(lowerLeft, lowerRight)
-		return dp[i][j]
-	}
-
-	return dfs(0, 0)
+	return dp[0][0]
 }
