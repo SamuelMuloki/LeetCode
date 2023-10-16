1f8d550f2ee8faba39680d61214704adf34e393a
Samuel Muloki <samuellebron0@gmail.com>
2023-10-16 14:37:34 +0300

    [Update] Add bottom up

--- go/solutions/1137-n-th-tribonacci-number.go
+++ go/solutions/1137-n-th-tribonacci-number.go
@@ -1,24 +1,18 @@
 package solutions
 
 func Tribonacci(n int) int {
-	dp := make(map[int]int, n)
-	var trib func(num int) int
-	trib = func(num int) int {
-		if num == 0 {
-			return 0
-		}
-
-		if num <= 2 {
-			return 1
-		}
-
-		if val, found := dp[num]; found {
-			return val
-		}
-
-		dp[num] = trib(num-3) + trib(num-2) + trib(num-1)
-		return dp[num]
+	if n == 0 {
+		return 0
 	}
 
-	return trib(n)
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	a, b, c := 0, 1, 1
+	for i := 3; i <= n; i++ {
+		a, b, c = b, c, a+b+c
+	}
+
+	return c
 }
