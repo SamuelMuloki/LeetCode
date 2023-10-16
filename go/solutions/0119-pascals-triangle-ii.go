3c2e91a2c7a7d1f9005f31e833d111cb5de6d1bb
Samuel Muloki <samuellebron0@gmail.com>
2023-10-16 11:39:08 +0300

    [Update] Add space optimized pascals triangle

--- go/solutions/0119-pascals-triangle-ii.go
+++ go/solutions/0119-pascals-triangle-ii.go
@@ -1,18 +1,13 @@
 package solutions
 
 func GetRow(rowIndex int) []int {
-	dp := make([][]int, rowIndex+1)
-	for i := range dp {
-		dp[i] = make([]int, rowIndex+1)
-	}
-
-	dp[0][0] = 1
-	for i := 1; i <= rowIndex; i++ {
-		dp[i][0] = 1
-		for j := 1; j < len(dp[i-1]); j++ {
-			dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+	dp := make([]int, rowIndex+1)
+	dp[0] = 1
+	for i := 0; i < rowIndex; i++ {
+		for j := i + 1; j > 0; j-- {
+			dp[j] += dp[j-1]
 		}
 	}
 
-	return dp[rowIndex]
+	return dp
 }
