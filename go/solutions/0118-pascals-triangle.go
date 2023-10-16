ed7ecdbe8850b47d0b882247e7735ff6e0ea9283
Samuel Muloki <samuellebron0@gmail.com>
2023-10-16 12:13:24 +0300

    [Update] Add optimized pascals triangle

--- go/solutions/0118-pascals-triangle.go
+++ go/solutions/0118-pascals-triangle.go
@@ -3,16 +3,12 @@ package solutions
 
 func Generate(numRows int) [][]int {
 	output := make([][]int, numRows)
-
-	output[0] = append(output[0], 1)
-	for i := 1; i < numRows; i++ {
-		newArr := []int{1}
-		for j := 1; j < len(output[i-1]); j++ {
-			newArr = append(newArr, output[i-1][j]+output[i-1][j-1])
+	for i := 0; i < numRows; i++ {
+		output[i] = make([]int, i+1)
+		output[i][0], output[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			output[i][j] = output[i-1][j-1] + output[i-1][j]
 		}
-
-		newArr = append(newArr, 1)
-		output[i] = append(output[i], newArr...)
 	}
 
 	return output
