1b90ccc9c4b5d2ce494f46b28d72f6065da0fe0c
Samuel Muloki <samuellebron0@gmail.com>
2023-10-17 10:03:53 +0300

    [Update] Add bottom min path sum

--- go/solutions/0064-minimum-path-sum.go
+++ go/solutions/0064-minimum-path-sum.go
@@ -12,29 +12,20 @@ func MinPathSum(grid [][]int) int {
 		dp[i] = make([]int, n)
 	}
 
-	for i := range dp {
-		for j := range dp[i] {
-			dp[i][j] = -1
+	dp[0][0] = grid[0][0]
+	for i := 1; i < n; i++ {
+		dp[0][i] = grid[0][i] + dp[0][i-1]
+	}
+
+	for i := 1; i < m; i++ {
+		dp[i][0] = grid[i][0] + dp[i-1][0]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = utils.Min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
 		}
 	}
 
-	var dfs func(i, j int) int
-	dfs = func(i, j int) int {
-		if i == m && j == n-1 {
-			return 0
-		}
-
-		if i == m || j == n {
-			return 1e9
-		}
-
-		if dp[i][j] != -1 {
-			return dp[i][j]
-		}
-
-		dp[i][j] = utils.Min(dfs(i+1, j), dfs(i, j+1)) + grid[i][j]
-		return dp[i][j]
-	}
-
-	return dfs(0, 0)
+	return dp[m-1][n-1]
 }
