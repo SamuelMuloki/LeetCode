6bc9efe8c3684cb93d8b31e02ce7bc7b1e12d126
Samuel Muloki <samuellebron0@gmail.com>
2023-10-17 17:37:38 +0300

    [Update] Add space optimised solution

--- go/solutions/0322-coin-change.go
+++ go/solutions/0322-coin-change.go
@@ -7,30 +7,22 @@ import (
 )
 
 func CoinChange(coins []int, amount int) int {
-	n := len(coins)
-	dp := make([][]int, n+1)
-
-	for i := range dp {
-		dp[i] = make([]int, amount+1)
-	}
-
+	dp := make([]int, amount+1)
 	for i := 1; i <= amount; i++ {
-		dp[0][i] = math.MaxInt32
+		dp[i] = math.MaxInt32
 	}
 
-	for i := 1; i <= n; i++ {
+	for _, coin := range coins {
 		for remain := 1; remain <= amount; remain++ {
-			if remain >= coins[i-1] {
-				dp[i][remain] = utils.Min(dp[i-1][remain], dp[i][remain-coins[i-1]]+1)
-			} else {
-				dp[i][remain] = dp[i-1][remain]
+			if remain >= coin {
+				dp[remain] = utils.Min(dp[remain], dp[remain-coin]+1)
 			}
 		}
 	}
 
-	if dp[n][amount] == math.MaxInt32 {
+	if dp[amount] == math.MaxInt32 {
 		return -1
 	}
 
-	return dp[n][amount]
+	return dp[amount]
 }
