36d5171c0928da9399d89b8e6ee71fc11d850ef9
Samuel Muloki <samuellebron0@gmail.com>
2023-10-18 15:41:34 +0300

    [Update] Add space optimised dp

--- go/solutions/0063-unique-paths-ii.go
+++ go/solutions/0063-unique-paths-ii.go
@@ -1,37 +1,19 @@
 package solutions
 
 func UniquePathsWithObstacles(obstacleGrid [][]int) int {
-	m, n := len(obstacleGrid), len(obstacleGrid[0])
-	dp := make([][]int, m)
-	for i := range dp {
-		dp[i] = make([]int, n)
-	}
+	n := len(obstacleGrid[0])
+	dp := make([]int, n)
 
-	for r := m - 1; r >= 0; r-- {
-		for c := n - 1; c >= 0; c-- {
-			if r == m-1 && c == n-1 {
-				if obstacleGrid[r][c] == 1 {
-					dp[r][c] = 0
-				} else {
-					dp[r][c] = 1
-				}
-			} else if r == m-1 || c == n-1 {
-				if obstacleGrid[r][c] == 1 {
-					dp[r][c] = 0
-				} else if r == m-1 {
-					dp[r][c] = dp[r][c+1]
-				} else if c == n-1 {
-					dp[r][c] = dp[r+1][c]
-				}
-			} else {
-				if obstacleGrid[r][c] == 1 {
-					dp[r][c] = 0
-				} else {
-					dp[r][c] = dp[r][c+1] + dp[r+1][c]
-				}
+	dp[0] = 1
+	for _, row := range obstacleGrid {
+		for j := 0; j < n; j++ {
+			if row[j] == 1 {
+				dp[j] = 0
+			} else if j > 0 {
+				dp[j] += dp[j-1]
 			}
 		}
 	}
 
-	return dp[0][0]
+	return dp[n-1]
 }
