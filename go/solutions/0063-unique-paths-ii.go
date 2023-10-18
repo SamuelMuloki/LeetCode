8067837e90b50c5277bfb8300eff3641ba20a27f
Samuel Muloki <samuellebron0@gmail.com>
2023-10-18 14:54:38 +0300

    [Update] Add unique paths bottom up

--- go/solutions/0063-unique-paths-ii.go
+++ go/solutions/0063-unique-paths-ii.go
@@ -2,31 +2,36 @@ package solutions
 
 func UniquePathsWithObstacles(obstacleGrid [][]int) int {
 	m, n := len(obstacleGrid), len(obstacleGrid[0])
-	dp := make([][]int, m+1)
+	dp := make([][]int, m)
 	for i := range dp {
-		dp[i] = make([]int, n+1)
+		dp[i] = make([]int, n)
 	}
 
-	var dfs func(r, c int) int
-	dfs = func(r, c int) int {
-		if r == m || c == n || obstacleGrid[r][c] == 1 {
-			return 0
-		}
-
-		if r == m-1 && c == n-1 {
-			if obstacleGrid[r][c] == 1 {
-				return 0
+	for r := m - 1; r >= 0; r-- {
+		for c := n - 1; c >= 0; c-- {
+			if r == m-1 && c == n-1 {
+				if obstacleGrid[r][c] == 1 {
+					dp[r][c] = 0
+				} else {
+					dp[r][c] = 1
+				}
+			} else if r == m-1 || c == n-1 {
+				if obstacleGrid[r][c] == 1 {
+					dp[r][c] = 0
+				} else if r == m-1 {
+					dp[r][c] = dp[r][c+1]
+				} else if c == n-1 {
+					dp[r][c] = dp[r+1][c]
+				}
+			} else {
+				if obstacleGrid[r][c] == 1 {
+					dp[r][c] = 0
+				} else {
+					dp[r][c] = dp[r][c+1] + dp[r+1][c]
+				}
 			}
-			return 1
 		}
-
-		if dp[r][c] != 0 {
-			return dp[r][c]
-		}
-
-		dp[r][c] = dfs(r+1, c) + dfs(r, c+1)
-		return dp[r][c]
 	}
 
-	return dfs(0, 0)
+	return dp[0][0]
 }
