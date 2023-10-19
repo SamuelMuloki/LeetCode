d56c0d00ecd905f9fcb1f2072420946e2efa6839
Samuel Muloki <samuellebron0@gmail.com>
2023-10-19 10:21:31 +0300

    [Update] Add two pointer approach

--- go/solutions/0844-backspace-string-compare.go
+++ go/solutions/0844-backspace-string-compare.go
@@ -1,26 +1,45 @@
 package solutions
 
 func BackspaceCompare(s string, t string) bool {
-	st1, st2 := make([]byte, 0), make([]byte, 0)
-	for i := range s {
-		if s[i] == '#' {
-			if len(st1) > 0 {
-				st1 = st1[:len(st1)-1]
+	i, j := len(s)-1, len(t)-1
+	skipS, skipT := 0, 0
+
+	for i >= 0 || j >= 0 {
+		for i >= 0 {
+			if s[i] == '#' {
+				skipS++
+				i--
+			} else if skipS > 0 {
+				skipS--
+				i--
+			} else {
+				break
 			}
-		} else {
-			st1 = append(st1, s[i])
 		}
+
+		for j >= 0 {
+			if t[j] == '#' {
+				skipT++
+				j--
+			} else if skipT > 0 {
+				skipT--
+				j--
+			} else {
+				break
+			}
+		}
+
+		if i >= 0 && j >= 0 && s[i] != t[j] {
+			return false
+		}
+
+		if (i >= 0) != (j >= 0) {
+			return false
+		}
+
+		i--
+		j--
 	}
 
-	for i := range t {
-		if t[i] == '#' {
-			if len(st2) > 0 {
-				st2 = st2[:len(st2)-1]
-			}
-		} else {
-			st2 = append(st2, t[i])
-		}
-	}
-
-	return string(st1) == string(st2)
+	return true
 }
