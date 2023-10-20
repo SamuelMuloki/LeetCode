b6b98437d3987f3b026888c7a0f696fbf7be8e17
Samuel Muloki <samuellebron0@gmail.com>
2023-10-20 13:50:04 +0300

    [Update] Remove seen map

--- go/solutions/0059-spiral-matrix-ii.go
+++ go/solutions/0059-spiral-matrix-ii.go
@@ -6,11 +6,6 @@ func GenerateMatrix(n int) [][]int {
 		ans[i] = make([]int, n)
 	}
 
-	seen := make([][]bool, n)
-	for i := range seen {
-		seen[i] = make([]bool, n)
-	}
-
 	dr, dc := []int{0, 1, 0, -1}, []int{1, 0, -1, 0}
 	x, y, di := 0, 0, 0
 	cnt := 1
@@ -18,10 +13,8 @@ func GenerateMatrix(n int) [][]int {
 		ans[x][y] = cnt
 		cnt++
 
-		seen[x][y] = true
 		newX, newY := x+dr[di], y+dc[di]
-
-		if 0 <= newX && newX < n && 0 <= newY && newY < n && !seen[newX][newY] {
+		if 0 <= newX && newX < n && 0 <= newY && newY < n && ans[newX][newY] == 0 {
 			x, y = newX, newY
 		} else {
 			di = (di + 1) % 4
