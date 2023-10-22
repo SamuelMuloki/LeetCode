93b70650dfdb5c1750d4f49d3ed7ca774b3b7bd9
Samuel Muloki <samuellebron0@gmail.com>
2023-10-22 12:46:35 +0300

    [Update] Add optimised solution

--- go/solutions/2908-minimum-sum-of-mountain-triplets-i.go
+++ go/solutions/2908-minimum-sum-of-mountain-triplets-i.go
@@ -2,28 +2,28 @@ package solutions
 
 import (
 	"math"
-	"sort"
 
 	"github.com/SamuelMuloki/LeetCode/go/utils"
 )
 
 func MinimumSum(nums []int) int {
-	res := math.MaxInt
 	n := len(nums)
-	arr := make([]int, 0)
-	for i := 0; i < n-1; i++ {
-		if len(arr) > 0 && arr[len(arr)-1] < nums[i] {
-			rem := nums[i+1:]
-			idx := sort.Search(len(rem), func(i int) bool {
-				return rem[i] < nums[i]
-			})
+	leftMin := make([]int, n)
+	leftMin[0] = nums[0]
+	for i := 1; i < n; i++ {
+		leftMin[i] = utils.Min(leftMin[i-1], nums[i])
+	}
 
-			if idx > -1 && rem[idx] < nums[i] {
-				res = utils.Min(res, arr[len(arr)-1]+nums[i]+rem[idx])
-			}
+	rightMin := make([]int, n)
+	rightMin[n-1] = nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		rightMin[i] = utils.Min(rightMin[i+1], nums[i])
+	}
 
-		} else {
-			arr = append(arr, nums[i])
+	res := math.MaxInt
+	for i := 1; i < n-1; i++ {
+		if nums[i] > leftMin[i-1] && nums[i] > rightMin[i+1] {
+			res = utils.Min(res, leftMin[i-1]+nums[i]+rightMin[i+1])
 		}
 	}
 
