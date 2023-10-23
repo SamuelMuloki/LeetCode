3cda693790b16c18217bb5d9c7f64aab45cdaf85
Samuel Muloki <samuellebron0@gmail.com>
2023-10-23 16:49:58 +0300

    [Update] Re arrange with one loop

--- go/solutions/2149-rearrange-array-elements-by-sign.go
+++ go/solutions/2149-rearrange-array-elements-by-sign.go
@@ -2,19 +2,14 @@ package solutions
 
 func RearrangeArray(nums []int) []int {
 	ans := make([]int, len(nums))
-	j := 0
+	pos, neg := 0, 1
 	for i := range nums {
 		if nums[i] > 0 {
-			ans[j] = nums[i]
-			j += 2
-		}
-	}
-
-	j = 1
-	for i := range nums {
-		if nums[i] < 0 {
-			ans[j] = nums[i]
-			j += 2
+			ans[pos] = nums[i]
+			pos += 2
+		} else {
+			ans[neg] = nums[i]
+			neg += 2
 		}
 	}
 
