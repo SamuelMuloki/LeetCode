77c85cb197908f096eb0adeca7a8919909d70b78
Samuel Muloki <samuellebron0@gmail.com>
2023-10-23 17:23:49 +0300

    [Update] Add more optimal approach

--- go/solutions/2161-partition-array-according-to-given-pivot.go
+++ go/solutions/2161-partition-array-according-to-given-pivot.go
@@ -1,26 +1,26 @@
 package solutions
 
 func PivotArray(nums []int, pivot int) []int {
-	var j int
+	l, p, r := 0, 0, len(nums)
+	for i := range nums {
+		if nums[i] < pivot {
+			p++
+		} else if nums[i] > pivot {
+			r--
+		}
+	}
+
 	ans := make([]int, len(nums))
 	for i := range nums {
 		if nums[i] < pivot {
-			ans[j] = nums[i]
-			j++
-		}
-	}
-
-	for i := range nums {
-		if nums[i] == pivot {
-			ans[j] = nums[i]
-			j++
-		}
-	}
-
-	for i := range nums {
-		if nums[i] > pivot {
-			ans[j] = nums[i]
-			j++
+			ans[l] = nums[i]
+			l++
+		} else if nums[i] == pivot {
+			ans[p] = nums[i]
+			p++
+		} else {
+			ans[r] = nums[i]
+			r++
 		}
 	}
 
