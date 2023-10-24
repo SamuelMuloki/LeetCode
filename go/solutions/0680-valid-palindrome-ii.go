689a8666d9d1b2b0bc3ba21bb5de95da62568a0d
Samuel Muloki <samuellebron0@gmail.com>
2023-10-24 13:30:54 +0300

    [Update] Add more efficient solution

--- go/solutions/0680-valid-palindrome-ii.go
+++ go/solutions/0680-valid-palindrome-ii.go
@@ -1,27 +1,31 @@
 package solutions
 
 func ValidPalindrome(s string) bool {
-	i, j := 0, len(s)-1
-	for i < j {
-		if s[i] != s[j] {
-			return validPalindromeUtil(s, i+1, j) || validPalindromeUtil(s, i, j-1)
-		}
-
-		i++
-		j--
+	valid, i, j := validPalindromeUtil(s, 0, len(s)-1)
+	if valid {
+		return true
 	}
 
-	return true
+	if valid, _, _ := validPalindromeUtil(s, i+1, j); valid {
+		return true
+	}
+
+	if valid, _, _ := validPalindromeUtil(s, i, j-1); valid {
+		return true
+	}
+
+	return false
 }
 
-func validPalindromeUtil(s string, i, j int) bool {
+func validPalindromeUtil(s string, i, j int) (bool, int, int) {
 	for i < j {
 		if s[i] != s[j] {
-			return false
+			return false, i, j
 		}
+
 		i++
 		j--
 	}
 
-	return true
+	return true, i, j
 }
