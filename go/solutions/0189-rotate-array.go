3814797bbdc739de81d8859f870df4c5be5b49de
Samuel Muloki <samuellebron0@gmail.com>
2023-10-25 14:20:57 +0300

    [Update] No extra space approach

--- go/solutions/0189-rotate-array.go
+++ go/solutions/0189-rotate-array.go
@@ -2,8 +2,16 @@ package solutions
 
 func RotateArray(nums []int, k int) {
 	n := len(nums)
-	arr := append([]int{}, nums...)
-	for i := 0; i < n; i++ {
-		nums[(i+k)%n] = arr[i]
+	k %= n
+	reverse2(nums, 0, n-1)
+	reverse2(nums, 0, k-1)
+	reverse2(nums, k, n-1)
+}
+
+func reverse2(nums []int, i, j int) {
+	for i < j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
 	}
 }
