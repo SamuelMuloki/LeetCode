f16c25ba42d5c551737bf6382deb862e6f10d6a3
Samuel Muloki <samuellebron0@gmail.com>
2023-10-27 15:19:18 +0300

    [Update] remove remain

--- go/solutions/0518-coin-change-ii.go
+++ go/solutions/0518-coin-change-ii.go
@@ -4,8 +4,8 @@ func Change(amount int, coins []int) int {
 	dp := make([]int, amount+1)
 	dp[0] = 1
 	for _, coin := range coins {
-		for remain := coin; remain <= amount; remain++ {
-			dp[remain] += dp[remain-coin]
+		for j := coin; j <= amount; j++ {
+			dp[j] += dp[j-coin]
 		}
 	}
 
