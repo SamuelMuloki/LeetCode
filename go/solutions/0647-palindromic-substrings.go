7c6cec85f105e9ed1e088f3e5a880455c6aed688
Samuel Muloki <samuellebron0@gmail.com>
2023-10-27 11:05:32 +0300

    [Update] Use dp

--- go/solutions/0647-palindromic-substrings.go
+++ go/solutions/0647-palindromic-substrings.go
@@ -1,14 +1,31 @@
 package solutions
 
 func CountSubstrings(s string) int {
-	count := 0
-	for i := 0; i < len(s); i++ {
-		count++
-		for j := i + 1; j < len(s); j++ {
-			str := string(s[i]) + string(s[i+1:j+1])
+	n, count := len(s), 0
+	dp := make([][]bool, n)
 
-			if isPalindrome(str) {
+	for i := 0; i < n; i++ {
+		dp[i] = make([]bool, n)
+	}
+
+	for i := 0; i < n; i++ {
+		count++
+		dp[i][i] = true
+	}
+
+	for i := 0; i < n-1; i++ {
+		if string(s[i]) == string(s[i+1]) {
+			count++
+			dp[i][i+1] = true
+		}
+	}
+
+	for diff := 2; diff < n; diff++ {
+		for i := 0; i < n-diff; i++ {
+			j := i + diff
+			if string(s[i]) == string(s[j]) && dp[i+1][j-1] {
 				count++
+				dp[i][j] = true
 			}
 		}
 	}
