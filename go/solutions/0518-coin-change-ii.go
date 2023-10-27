f4b988dc9f49ce14a2477087410e38419433ad69
Samuel Muloki <samuellebron0@gmail.com>
2023-10-27 15:17:37 +0300

    [Update] Add bottom up solution

--- go/solutions/0518-coin-change-ii.go
+++ go/solutions/0518-coin-change-ii.go
@@ -1,35 +1,13 @@
 package solutions
 
-type Pair struct {
-	a, b int
-}
-
 func Change(amount int, coins []int) int {
-	n := len(coins)
-	dp := make(map[Pair]int, n)
-
-	var dfs func(i, val int) int
-	dfs = func(i, val int) int {
-		pair := Pair{i, val}
-		if v, ok := dp[pair]; ok {
-			return v
+	dp := make([]int, amount+1)
+	dp[0] = 1
+	for _, coin := range coins {
+		for remain := coin; remain <= amount; remain++ {
+			dp[remain] += dp[remain-coin]
 		}
-
-		if val == amount {
-			return 1
-		}
-
-		if val > amount {
-			return 0
-		}
-
-		if i == len(coins) {
-			return 0
-		}
-
-		dp[pair] = dfs(i+1, val) + dfs(i, val+coins[i])
-		return dp[pair]
 	}
 
-	return dfs(0, 0)
+	return dp[amount]
 }
