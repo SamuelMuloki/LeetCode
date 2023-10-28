4069db8d8cbba0f29fcf8de22eebfabe861bb4ac
Samuel Muloki <samuellebron0@gmail.com>
2023-10-28 11:30:44 +0300

    [Update] Add space optimised DP

--- go/solutions/1220-count-vowels-permutation.go
+++ go/solutions/1220-count-vowels-permutation.go
@@ -2,24 +2,13 @@ package solutions
 
 func CountVowelPermutation(n int) int {
 	mod := 1000000007
-	dp := make([][]int, 5)
-	for i := range dp {
-		dp[i] = make([]int, n+1)
+	a, e, i, o, u := 1, 1, 1, 1, 1
+	for j := 1; j < n; j++ {
+		aNext, eNext, iNext := e, (a+i)%mod, (a+e+o+u)%mod
+		oNext, uNext := (i+u)%mod, a
+
+		a, e, i, o, u = aNext, eNext, iNext, oNext, uNext
 	}
 
-	dp[0][1], dp[1][1], dp[2][1], dp[3][1], dp[4][1] = 1, 1, 1, 1, 1
-	for i := 2; i <= n; i++ {
-		dp[0][i] = dp[1][i-1]
-		dp[1][i] = (dp[0][i-1] + dp[2][i-1]) % mod
-		dp[2][i] = (dp[0][i-1] + dp[1][i-1] + dp[3][i-1] + dp[4][i-1]) % mod
-		dp[3][i] = (dp[2][i-1] + dp[4][i-1]) % mod
-		dp[4][i] = dp[0][i-1]
-	}
-
-	ans := 0
-	for i := 0; i < 5; i++ {
-		ans = (ans + dp[i][n]) % mod
-	}
-
-	return ans
+	return (a + e + i + o + u) % mod
 }
