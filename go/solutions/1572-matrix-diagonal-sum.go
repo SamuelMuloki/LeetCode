f7f008378ce14d586c56cdd8e455fb6c6a02b153
Samuel Muloki <samuellebron0@gmail.com>
2023-10-30 13:27:07 +0300

    [Update] Use single loop

--- go/solutions/1572-matrix-diagonal-sum.go
+++ go/solutions/1572-matrix-diagonal-sum.go
@@ -1,15 +1,18 @@
 package solutions
 
 func DiagonalSum(mat [][]int) int {
-	sum := 0
-	for i := 0; i < len(mat); i++ {
-		sum += mat[i][i]
-		mat[i][i] = 0
+	ans, n := 0, len(mat)
+	for i := 0; i < n; i++ {
+		// Add elements from primary diagonal.
+		ans += mat[i][i]
+		// Add elements from secondary diagonal.
+		ans += mat[n-1-i][i]
 	}
 
-	for i, j := 0, len(mat)-1; i < len(mat); i, j = i+1, j-1 {
-		sum += mat[i][j]
+	// If n is odd, subtract the middle element as its added twice.
+	if n&1 != 0 {
+		ans -= mat[n/2][n/2]
 	}
 
-	return sum
+	return ans
 }
