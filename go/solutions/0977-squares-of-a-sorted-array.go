c28c8141df6b2fb30d3c7794bf95b0555ee5dc82
Samuel Muloki <samuellebron0@gmail.com>
2023-10-30 23:06:11 +0300

    [Update] Use 2 pointers

--- go/solutions/0977-squares-of-a-sorted-array.go
+++ go/solutions/0977-squares-of-a-sorted-array.go
@@ -1,13 +1,20 @@
 package solutions
 
-import "sort"
-
 func SortedSquares(nums []int) []int {
-	ans := make([]int, len(nums))
-	for i := 0; i < len(nums); i++ {
-		ans[i] = nums[i] * nums[i]
+	n := len(nums)
+	ans := make([]int, n)
+	start, end := 0, n-1
+	for k := n - 1; start <= end; k-- {
+		startProd := nums[start] * nums[start]
+		endProd := nums[end] * nums[end]
+		if startProd > endProd {
+			ans[k] = startProd
+			start++
+		} else {
+			ans[k] = endProd
+			end--
+		}
 	}
 
-	sort.Ints(ans)
 	return ans
 }
