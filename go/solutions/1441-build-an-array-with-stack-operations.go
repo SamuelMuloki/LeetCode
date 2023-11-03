d3ff73d11a20f4fd72eb73e29ab321e0f4424ed0
Samuel Muloki <samuellebron0@gmail.com>
2023-11-03 10:12:52 +0300

    [Update] Add shorter solution

--- go/solutions/1441-build-an-array-with-stack-operations.go
+++ go/solutions/1441-build-an-array-with-stack-operations.go
@@ -1,26 +1,17 @@
 package solutions
 
 func BuildArray(target []int, n int) []string {
-	st := make([]int, 0)
-	ops := make([]string, 0)
+	ans := make([]string, 0)
 	j := 0
-	for i := 1; i <= n; i++ {
-		if len(st) > 0 {
-			if st[len(st)-1] != target[j] {
-				st = st[:len(st)-1]
-				ops = append(ops, "Pop")
-			} else {
-				j++
-			}
+	for i := 1; j < len(target) && i <= n; i++ {
+		if target[j] == i {
+			j++
+			ans = append(ans, "Push")
+		} else {
+			ans = append(ans, "Push")
+			ans = append(ans, "Pop")
 		}
-
-		if j >= len(target) {
-			break
-		}
-
-		st = append(st, i)
-		ops = append(ops, "Push")
 	}
 
-	return ops
+	return ans
 }
