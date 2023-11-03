96eac646732c6be00b1befcd54956ba7aa3adf6d
Samuel Muloki <samuellebron0@gmail.com>
2023-11-03 12:20:14 +0300

    [Update] Use more efficient solution

--- go/solutions/1209-remove-all-adjacent-duplicates-in-string-ii.go
+++ go/solutions/1209-remove-all-adjacent-duplicates-in-string-ii.go
@@ -1,18 +1,29 @@
 package solutions
 
-func RemoveDuplicates4(s string, k int) string {
-	st := []byte{}
-	for i := range s {
-		count := 1
-		for ; len(st) >= count && s[i] == st[len(st)-count]; count++ {
-		}
+import "bytes"
 
-		if count == k {
-			st = st[:len(st)-count+1]
+type Pair struct {
+	ch   byte
+	freq int
+}
+
+func RemoveDuplicates4(s string, k int) string {
+	st := make([]Pair, 0)
+	for i := range s {
+		if len(st) > 0 && st[len(st)-1].ch == s[i] {
+			st[len(st)-1].freq++
+			if st[len(st)-1].freq == k {
+				st = st[:len(st)-1]
+			}
 		} else {
-			st = append(st, s[i])
+			st = append(st, Pair{s[i], 1})
 		}
 	}
 
-	return string(st)
+	var ans string
+	for i := range st {
+		ans += string(bytes.Repeat([]byte{st[i].ch}, st[i].freq))
+	}
+
+	return ans
 }
