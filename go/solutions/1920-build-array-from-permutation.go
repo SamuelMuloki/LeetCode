210be21f244756364b2150fd1be0012cdf1d66de
Samuel Muloki <samuellebron0@gmail.com>
2023-11-03 19:35:49 +0300

    [Update] Add some notes

--- go/solutions/1920-build-array-from-permutation.go
+++ go/solutions/1920-build-array-from-permutation.go
@@ -2,7 +2,12 @@ package solutions
 
 func BuildArray2(nums []int) []int {
 	n := len(nums)
+	// turn the array into a=nb+r
 	for j := range nums {
+		/* retrieve correct value from potentially already processed element
+		i.e. get r(nums[j]) from something maybe already in the form a = nb + r
+		if it isnt already processed (doesnt have nb yet), that's ok b/c
+		r < n, so r % n will return the same value */
 		nums[j] = nums[j] + n*(nums[nums[j]]%n)
 	}
 
