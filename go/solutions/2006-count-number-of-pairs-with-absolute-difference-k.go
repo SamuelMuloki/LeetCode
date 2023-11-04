e2a6f84f41cb7b9584ac378ff4d111757c923794
Samuel Muloki <samuellebron0@gmail.com>
2023-11-04 16:37:29 +0300

    [Update] Add more efficient solution

--- go/solutions/2006-count-number-of-pairs-with-absolute-difference-k.go
+++ go/solutions/2006-count-number-of-pairs-with-absolute-difference-k.go
@@ -1,13 +1,17 @@
 package solutions
 
+/*
+Since nums[i] is in the [1...100] range, we can count each number using an array.
+Now, we can sweep the counts, and accumulate the product of k-apart counts.
+*/
 func CountKDifference(nums []int, k int) int {
-	ans := 0
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if abs(nums[i]-nums[j]) == k {
-				ans++
-			}
-		}
+	ans, cnt := 0, make([]int, 101)
+	for i := range nums {
+		cnt[nums[i]]++
+	}
+
+	for i := k + 1; i < 101; i++ {
+		ans += cnt[i] * cnt[i-k]
 	}
 
 	return ans
