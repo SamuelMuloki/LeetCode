a855ef7db4b79959a3c4d9108685a9f37267a193
Samuel Muloki <samuellebron0@gmail.com>
2023-11-04 16:58:55 +0300

    [Update] Add More efficient solution

--- go/solutions/0832-flipping-an-image.go
+++ go/solutions/0832-flipping-an-image.go
@@ -2,14 +2,8 @@ package solutions
 
 func FlipAndInvertImage(image [][]int) [][]int {
 	for i := range image {
-		for k, j := 0, len(image[i])-1; k < j; k, j = k+1, j-1 {
-			image[i][k], image[i][j] = image[i][j], image[i][k]
-		}
-	}
-
-	for i := range image {
-		for j := range image[i] {
-			image[i][j] ^= 1
+		for k, j := 0, len(image[i])-1; k <= j; k, j = k+1, j-1 {
+			image[i][k], image[i][j] = image[i][j]^1, image[i][k]^1
 		}
 	}
 
