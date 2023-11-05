f2a561ddb8e3ada26491ab6239adbfd356af3033
Samuel Muloki <samuellebron0@gmail.com>
2023-11-05 13:27:09 +0300

    [Update] Add different solution

--- go/solutions/0461-hamming-distance.go
+++ go/solutions/0461-hamming-distance.go
@@ -2,10 +2,9 @@ package solutions
 
 func HammingDistance(x int, y int) int {
 	count := 0
-	for i := x ^ y; i != 0; i >>= 1 {
-		if i&1 == 1 {
-			count++
-		}
+	// n & (n-1) converts the right most 1 to 0
+	for n := x ^ y; n != 0; n &= n - 1 {
+		count++
 	}
 
 	return count
