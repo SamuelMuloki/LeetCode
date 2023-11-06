4eaf972f9fb8f5d8dd3d09e9dd03608eb9b1a6e5
Samuel Muloki <samuellebron0@gmail.com>
2023-11-06 14:31:55 +0300

    [Update] Add more optimised solution

--- go/solutions/0496-next-greater-element-i.go
+++ go/solutions/0496-next-greater-element-i.go
@@ -1,23 +1,20 @@
 package solutions
 
 func NextGreaterElement(nums1 []int, nums2 []int) []int {
-	ans := make([]int, len(nums1))
-	for i := range ans {
-		ans[i] = -1
+	ng, st := map[int]int{}, []int{}
+	for _, num := range nums2 {
+		ng[num] = -1
+		for len(st) > 0 && st[len(st)-1] < num {
+			top := st[len(st)-1]
+			st = st[:len(st)-1]
+			ng[top] = num
+		}
+		st = append(st, num)
 	}
 
-	for i, num := range nums1 {
-		found := false
-		for j := 0; j < len(nums2); j++ {
-			if nums2[j] == num {
-				found = true
-			}
-
-			if found && nums2[j] > num {
-				ans[i] = nums2[j]
-				break
-			}
-		}
+	ans := []int{}
+	for _, num := range nums1 {
+		ans = append(ans, ng[num])
 	}
 
 	return ans
