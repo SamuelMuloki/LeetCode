5545a6b697b5f536abca5099b35128b2d904bfaf
Samuel Muloki <samuellebron0@gmail.com>
2023-11-06 11:49:26 +0300

    [Update] Sort by end

--- go/solutions/0452-minimum-number-of-arrows-to-burst-balloons.go
+++ go/solutions/0452-minimum-number-of-arrows-to-burst-balloons.go
@@ -2,25 +2,21 @@ package solutions
 
 import (
 	"sort"
-
-	"github.com/SamuelMuloki/LeetCode/go/utils"
 )
 
 func FindMinArrowShots(points [][]int) int {
-	overlaps := make([][]int, 0)
 	sort.Slice(points, func(i, j int) bool {
-		return points[i][0] < points[j][0]
+		return points[i][1] < points[j][1]
 	})
 
-	overlaps = append(overlaps, points[0])
+	ans, arrowPos := 1, points[0][1]
 	for i := 1; i < len(points); i++ {
-		n := len(overlaps)
-		if points[i][0] <= overlaps[n-1][1] {
-			overlaps[n-1][1] = utils.Min(overlaps[n-1][1], points[i][1])
-		} else {
-			overlaps = append(overlaps, points[i])
+		if arrowPos >= points[i][0] {
+			continue
 		}
+		ans++
+		arrowPos = points[i][1]
 	}
 
-	return len(overlaps)
+	return ans
 }
