2ee76f7fa866ae3026e59cb1b6ef65e3543a7ce5
Samuel Muloki <samuellebron0@gmail.com>
2023-11-07 12:37:24 +0300

    [Update] Add heap solution

--- go/solutions/1921-eliminate-maximum-number-of-monsters.go
+++ go/solutions/1921-eliminate-maximum-number-of-monsters.go
@@ -1,21 +1,22 @@
 package solutions
 
-import "sort"
+import "container/heap"
 
 func EliminateMaximum(dist []int, speed []int) int {
-	arrival := make([]float64, 0)
+	h := &MinHeap{}
+	heap.Init(h)
 	for i := 0; i < len(dist); i++ {
-		arrival = append(arrival, float64(dist[i])/float64(speed[i]))
+		heap.Push(h, (dist[i]-1)/speed[i])
 	}
-	sort.Float64s(arrival)
 
 	ans := 0
-	for i := range arrival {
-		if arrival[i] <= float64(i) {
+	for h.Len() > 0 {
+		if (*h)[0] < ans {
 			break
 		}
 
 		ans++
+		heap.Pop(h)
 	}
 
 	return ans
