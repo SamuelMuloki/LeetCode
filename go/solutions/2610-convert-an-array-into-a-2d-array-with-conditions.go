362f03d43840d6a4b887c4831bec85abc609b9fe
Samuel Muloki <samuellebron0@gmail.com>
2023-11-07 14:11:42 +0300

    [Update] Add approach 3

--- go/solutions/2610-convert-an-array-into-a-2d-array-with-conditions.go
+++ go/solutions/2610-convert-an-array-into-a-2d-array-with-conditions.go
@@ -1,15 +1,19 @@
 package solutions
 
-func FindMatrix(nums []int) [][]int {
-	freq := make([]int, len(nums)+1)
-	ans := [][]int{}
-	for _, num := range nums {
-		if freq[num] >= len(ans) {
-			ans = append(ans, []int{})
-		}
+import "github.com/SamuelMuloki/LeetCode/go/utils"
 
-		ans[freq[num]] = append(ans[freq[num]], num)
-		freq[num]++
+func FindMatrix(nums []int) [][]int {
+	cnt, maxCnt := [201]int{}, 0
+	for i := range nums {
+		cnt[nums[i]]++
+		maxCnt = utils.Max(maxCnt, cnt[nums[i]])
+	}
+
+	ans := make([][]int, maxCnt)
+	for i := 0; i < 201; i++ {
+		for j := 0; j < cnt[i]; j++ {
+			ans[j] = append(ans[j], i)
+		}
 	}
 
 	return ans
