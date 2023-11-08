d1e227826ac7bbb75a126a34597a02873f8428dc
Samuel Muloki <samuellebron0@gmail.com>
2023-11-08 12:55:16 +0300

    [Update] Use O(1) time

--- go/solutions/1486-xor-operation-in-an-array.go
+++ go/solutions/1486-xor-operation-in-an-array.go
@@ -1,10 +1,27 @@
 package solutions
 
+// https://leetcode.com/problems/xor-operation-in-an-array/solutions/699141/visual-solution-python-o-1-time-o-1-space/
 func XorOperation(n int, start int) int {
-	ans := 0
-	for i := 0; i < n; i++ {
-		ans ^= start + 2*i
+	last := start + 2*(n-1)
+	if start%4 <= 1 {
+		if n%4 == 1 {
+			return last
+		} else if n%4 == 2 {
+			return 2
+		} else if n%4 == 3 {
+			return 2 ^ last
+		} else {
+			return 0
+		}
 	}
 
-	return ans
+	if n%4 == 1 {
+		return start
+	} else if n%4 == 2 {
+		return start ^ last
+	} else if n%4 == 3 {
+		return start ^ 2
+	}
+
+	return start ^ 2 ^ last
 }
