b5ed0fa626d3d951da768874b68202c5d1f6fb82
Samuel Muloki <samuellebron0@gmail.com>
2023-11-10 13:55:33 +0300

    [Update] Add iterative method

--- go/solutions/1315-sum-of-nodes-with-even-valued-grandparent.go
+++ go/solutions/1315-sum-of-nodes-with-even-valued-grandparent.go
@@ -10,19 +10,43 @@ import "github.com/SamuelMuloki/LeetCode/go/utils"
  *     Right *TreeNode
  * }
  */
-func SumEvenGrandparent(root *utils.TreeNode) int {
-	return findSum(root, -1, -1)
+
+type GrandNode struct {
+	node            *utils.TreeNode
+	parent, gParent int
 }
 
-func findSum(root *utils.TreeNode, parent, gParent int) int {
-	if root == nil {
-		return 0
+func SumEvenGrandparent(root *utils.TreeNode) int {
+	queue := make([]*GrandNode, 0)
+	queue = append(queue, &GrandNode{root, -1, -1})
+
+	var ans int
+	for len(queue) > 0 {
+		qLen := len(queue)
+		for i := 0; i < qLen; i++ {
+			if queue[i].gParent%2 == 0 {
+				ans += queue[i].node.Val
+			}
+
+			if queue[i].node.Left != nil {
+				queue = append(queue, &GrandNode{
+					node:    queue[i].node.Left,
+					parent:  queue[i].node.Val,
+					gParent: queue[i].parent,
+				})
+			}
+
+			if queue[i].node.Right != nil {
+				queue = append(queue, &GrandNode{
+					node:    queue[i].node.Right,
+					parent:  queue[i].node.Val,
+					gParent: queue[i].parent,
+				})
+			}
+		}
+
+		queue = queue[qLen:]
 	}
 
-	val := 0
-	if gParent%2 == 0 {
-		val = root.Val
-	}
-
-	return findSum(root.Left, root.Val, parent) + findSum(root.Right, root.Val, parent) + val
+	return ans
 }
