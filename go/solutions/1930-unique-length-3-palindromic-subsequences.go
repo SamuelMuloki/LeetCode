42f717af33547ebd21e531fc6c020f0c49165e9a
Samuel Muloki <samuellebron0@gmail.com>
2023-11-14 11:49:37 +0300

    [Update] Add optimised solution

--- go/solutions/1930-unique-length-3-palindromic-subsequences.go
+++ go/solutions/1930-unique-length-3-palindromic-subsequences.go
@@ -1,27 +1,29 @@
 package solutions
 
 func CountPalindromicSubsequence(s string) int {
-	letters := make(map[byte]bool)
-	for i := range s {
-		letters[s[i]] = true
+	first, last := [26]int{}, [26]int{}
+	for i := 0; i < 26; i++ {
+		first[i], last[i] = -1, -1
+	}
+
+	for i := 0; i < len(s); i++ {
+		curr := s[i] - 'a'
+		if first[curr] == -1 {
+			first[curr] = i
+		}
+
+		last[curr] = i
 	}
 
 	ans := 0
-	for letter := range letters {
-		i, j := -1, 0
-		for k := 0; k < len(s); k++ {
-			if s[k] == letter {
-				if i == -1 {
-					i = k
-				}
-
-				j = k
-			}
+	for i := 0; i < 26; i++ {
+		if first[i] == -1 {
+			continue
 		}
 
 		between := make(map[byte]bool)
-		for k := i + 1; k < j; k++ {
-			between[s[k]] = true
+		for j := first[i] + 1; j < last[i]; j++ {
+			between[s[j]] = true
 		}
 
 		ans += len(between)
