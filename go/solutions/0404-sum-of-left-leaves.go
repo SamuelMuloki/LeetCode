3257e8d610d84062d6b8242d73f00cf2d898d743
Samuel Muloki <samuellebron0@gmail.com>
2023-11-17 12:35:42 +0300

    [Update] Add sum of left leaves

---
+++ go/solutions/0404-sum-of-left-leaves.go
@@ -0,0 +1,31 @@
+package solutions
+
+import "github.com/SamuelMuloki/LeetCode/go/utils"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func SumOfLeftLeaves(root *utils.TreeNode) int {
+	return dfs2(root, false)
+}
+
+func dfs2(root *utils.TreeNode, isLeft bool) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil && root.Right == nil {
+		if isLeft {
+			return root.Val
+		} else {
+			return 0
+		}
+	}
+
+	return dfs2(root.Left, true) + dfs2(root.Right, false)
+}
