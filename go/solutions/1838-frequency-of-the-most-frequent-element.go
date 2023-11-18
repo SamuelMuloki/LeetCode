a254534863cfdd9d1a5b5ccc14cf08926089422e
Samuel Muloki <samuellebron0@gmail.com>
2023-11-18 12:13:27 +0300

    [Update] Add frequencey of the most frequency elem

---
+++ go/solutions/1838-frequency-of-the-most-frequent-element.go
@@ -0,0 +1,21 @@
+package solutions
+
+import "sort"
+
+func MaxFrequency(nums []int, k int) int {
+	sort.Ints(nums)
+	left, ans, curr := 0, 0, 0
+	for right := 0; right < len(nums); right++ {
+		target := nums[right]
+		curr += target
+
+		for (right-left+1)*target-curr > k {
+			curr -= nums[left]
+			left++
+		}
+
+		ans = max(ans, right-left+1)
+	}
+
+	return ans
+}
