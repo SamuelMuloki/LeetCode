69ccd23cb1564e258e89142449b4e4c0a4b65cc6
Samuel Muloki <samuellebron0@gmail.com>
2023-11-18 15:16:04 +0300

    [Update] Sort array first

--- go/solutions/2824-count-pairs-whose-sum-is-less-than-target.go
+++ go/solutions/2824-count-pairs-whose-sum-is-less-than-target.go
@@ -1,12 +1,16 @@
 package solutions
 
+import "sort"
+
 func CountPairs(nums []int, target int) int {
-	ans := 0
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] < target {
-				ans++
-			}
+	sort.Ints(nums)
+	ans, left, right := 0, 0, len(nums)-1
+	for left < right {
+		if nums[left]+nums[right] < target {
+			ans += right - left
+			left++
+		} else {
+			right--
 		}
 	}
 
