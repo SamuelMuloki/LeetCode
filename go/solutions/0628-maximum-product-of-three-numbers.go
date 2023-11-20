663b0f46b05a70842d992704772e7b36404e3fa6
Samuel Muloki <samuellebron0@gmail.com>
2023-11-20 13:41:21 +0300

    [Update] More optimised solution

--- go/solutions/0628-maximum-product-of-three-numbers.go
+++ go/solutions/0628-maximum-product-of-three-numbers.go
@@ -1,9 +1,26 @@
 package solutions
 
-import "sort"
+import "math"
 
 func MaximumProduct(nums []int) int {
-	sort.Ints(nums)
-	n := len(nums)
-	return max(nums[0]*nums[1]*nums[n-1], nums[n-3]*nums[n-2]*nums[n-1])
+	max1, max2, max3 := math.MinInt32, math.MinInt32, math.MinInt32
+	min1, min2 := math.MaxInt32, math.MaxInt32
+
+	for i := range nums {
+		if nums[i] > max1 {
+			max1, max2, max3 = nums[i], max1, max2
+		} else if nums[i] > max2 {
+			max2, max3 = nums[i], max2
+		} else if nums[i] > max3 {
+			max3 = nums[i]
+		}
+
+		if nums[i] < min1 {
+			min1, min2 = nums[i], min1
+		} else if nums[i] < min2 {
+			min2 = nums[i]
+		}
+	}
+
+	return max(max1*max2*max3, min1*min2*max1)
 }
