bf76885292f567c6d9b61ca49924fc8b39a48e90
Samuel Muloki <samuellebron0@gmail.com>
2023-11-20 11:37:41 +0300

    [Update] In place modification

--- go/solutions/2391-minimum-amount-of-time-to-collect-garbage.go
+++ go/solutions/2391-minimum-amount-of-time-to-collect-garbage.go
@@ -1,26 +1,24 @@
 package solutions
 
 func GarbageCollection(garbage []string, travel []int) int {
-	prefixSum := make([]int, len(travel)+1)
-	prefixSum[1] = travel[0]
 	for i := 1; i < len(travel); i++ {
-		prefixSum[i+1] = prefixSum[i] + travel[i]
+		travel[i] = travel[i-1] + travel[i]
 	}
 
-	garbageLastPos := make(map[byte]int)
-	garbageCount := make(map[byte]int)
-	for i := range garbage {
-		for j := range garbage[i] {
-			garbageLastPos[garbage[i][j]] = i
-			garbageCount[garbage[i][j]]++
-		}
-	}
-
-	garbageTypes := []byte{'M', 'P', 'G'}
+	garbageLastPos := make(map[rune]int)
 	ans := 0
+	for i := range garbage {
+		for _, ch := range garbage[i] {
+			garbageLastPos[ch] = i
+		}
+
+		ans += len(garbage[i])
+	}
+
+	garbageTypes := "MPG"
 	for _, ch := range garbageTypes {
-		if count, ok := garbageCount[ch]; ok {
-			ans += prefixSum[garbageLastPos[ch]] + count
+		if garbageLastPos[ch] != 0 {
+			ans += travel[garbageLastPos[ch]-1]
 		}
 	}
 
