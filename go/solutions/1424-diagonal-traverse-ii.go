e35407e59e6ce9c7375675cbaf212e4c74aff49d
Samuel Muloki <samuellebron0@gmail.com>
2023-11-22 13:40:59 +0300

    [Update] Add BFS approach

--- go/solutions/1424-diagonal-traverse-ii.go
+++ go/solutions/1424-diagonal-traverse-ii.go
@@ -1,19 +1,25 @@
 package solutions
 
-func FindDiagonalOrder(nums [][]int) []int {
-	groups := make(map[int][]int)
-	for row := len(nums) - 1; row >= 0; row-- {
-		for col := 0; col < len(nums[row]); col++ {
-			diagonal := row + col
-			groups[diagonal] = append(groups[diagonal], nums[row][col])
-		}
-	}
+type DiagonalPair struct {
+	row, col int
+}
 
+func FindDiagonalOrder(nums [][]int) []int {
+	queue := make([]DiagonalPair, 0)
+	queue = append(queue, DiagonalPair{0, 0})
 	ans := make([]int, 0)
-	curr := 0
-	for len(groups[curr]) > 0 {
-		ans = append(ans, groups[curr]...)
-		curr++
+	for len(queue) > 0 {
+		row, col := queue[0].row, queue[0].col
+		queue = queue[1:]
+		ans = append(ans, nums[row][col])
+
+		if col == 0 && row+1 < len(nums) {
+			queue = append(queue, DiagonalPair{row: row + 1, col: col})
+		}
+
+		if col+1 < len(nums[row]) {
+			queue = append(queue, DiagonalPair{row: row, col: col + 1})
+		}
 	}
 
 	return ans
