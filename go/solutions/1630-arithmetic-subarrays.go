872a2694f83e98e497b060b3b507a212f9df2db9
Samuel Muloki <samuellebron0@gmail.com>
2023-11-23 12:51:20 +0300

    [Update] Add without sorting

--- go/solutions/1630-arithmetic-subarrays.go
+++ go/solutions/1630-arithmetic-subarrays.go
@@ -1,20 +1,39 @@
 package solutions
 
-import "sort"
+import "math"
 
 func CheckArithmeticSubarrays(nums []int, l []int, r []int) []bool {
 	ans := make([]bool, len(l))
 	for i := 0; i < len(l); i++ {
-		sub := append([]int{}, nums[l[i]:r[i]+1]...)
-		sort.Ints(sub)
-		ans[i] = true
-		for j := 2; j < len(sub); j++ {
-			if sub[j]-sub[j-1] != sub[1]-sub[0] {
-				ans[i] = false
-				break
-			}
-		}
+		ans[i] = checkArithmeticSub(nums[l[i] : r[i]+1])
 	}
 
 	return ans
+}
+
+func checkArithmeticSub(nums []int) bool {
+	minELement, maxELement := math.MaxInt, math.MinInt
+	set := make(map[int]int)
+
+	for i := range nums {
+		minELement = min(minELement, nums[i])
+		maxELement = max(maxELement, nums[i])
+		set[nums[i]]++
+	}
+
+	if (maxELement-minELement)%(len(nums)-1) != 0 {
+		return false
+	}
+
+	diff := (maxELement - minELement) / (len(nums) - 1)
+	curr := minELement + diff
+	for curr < maxELement {
+		if _, ok := set[curr]; !ok {
+			return false
+		}
+
+		curr += diff
+	}
+
+	return true
 }
