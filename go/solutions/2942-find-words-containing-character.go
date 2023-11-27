55eb843a0e6ed5f8b061a8aa472e4ddd90339b82
Samuel Muloki <samuellebron0@gmail.com>
2023-11-27 15:05:06 +0300

    [Update] Use index byte

--- go/solutions/2942-find-words-containing-character.go
+++ go/solutions/2942-find-words-containing-character.go
@@ -1,17 +1,11 @@
 package solutions
 
+import "strings"
+
 func FindWordsContaining(words []string, x byte) []int {
 	ans := make([]int, 0)
 	for i := range words {
-		found := false
-		for j := range words[i] {
-			if words[i][j] == x {
-				found = true
-				break
-			}
-		}
-
-		if found {
+		if strings.IndexByte(words[i], x) > -1 {
 			ans = append(ans, i)
 		}
 	}
