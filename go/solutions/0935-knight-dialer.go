b9e12c1b44fde156a6f3a14ad22a6231219592cd
Samuel Muloki <samuellebron0@gmail.com>
2023-11-27 12:00:59 +0300

    [Update] Add bottom up solution

--- go/solutions/0935-knight-dialer.go
+++ go/solutions/0935-knight-dialer.go
@@ -1,9 +1,13 @@
 package solutions
 
 func KnightDialer(n int) int {
-	memo := make([][]int, n+1)
-	for i := range memo {
-		memo[i] = make([]int, 10)
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, 10)
+	}
+
+	for square := 0; square < 10; square++ {
+		dp[0][square] = 1
 	}
 
 	jumps := [][]int{
@@ -11,28 +15,19 @@ func KnightDialer(n int) int {
 		{}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4},
 	}
 
-	var dp func(remain, square int) int
-	dp = func(remain, square int) int {
-		if remain == 0 {
-			return 1
+	for remain := 1; remain < n; remain++ {
+		for square := 0; square < 10; square++ {
+			ans := 0
+			for _, nextSquare := range jumps[square] {
+				ans = (ans + dp[remain-1][nextSquare]) % (1e9 + 7)
+			}
+			dp[remain][square] = ans
 		}
-
-		if memo[remain][square] != 0 {
-			return memo[remain][square]
-		}
-
-		ans := 0
-		for _, nextSquare := range jumps[square] {
-			ans = (ans + dp(remain-1, nextSquare)) % (1e9 + 7)
-		}
-
-		memo[remain][square] = ans
-		return ans
 	}
 
 	ans := 0
 	for square := 0; square < 10; square++ {
-		ans = (ans + dp(n-1, square)) % (1e9 + 7)
+		ans = (ans + dp[n-1][square]) % (1e9 + 7)
 	}
 
 	return ans
