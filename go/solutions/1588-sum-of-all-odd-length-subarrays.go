d9d1502bd00be852a87f7d4400b71576d4f28c25
Samuel Muloki <samuellebron0@gmail.com>
2023-11-28 13:42:58 +0300

    [Update] More optimised solution

--- go/solutions/1588-sum-of-all-odd-length-subarrays.go
+++ go/solutions/1588-sum-of-all-odd-length-subarrays.go
@@ -3,12 +3,10 @@ package solutions
 func SumOddLengthSubarrays(arr []int) int {
 	n, ans := len(arr), 0
 	for left := 0; left < n; left++ {
+		currSum := 0
 		for right := left; right < n; right++ {
+			currSum += arr[right]
 			if (right-left+1)%2 == 1 {
-				currSum := 0
-				for i := left; i < right+1; i++ {
-					currSum += arr[i]
-				}
 				ans += currSum
 			}
 		}
