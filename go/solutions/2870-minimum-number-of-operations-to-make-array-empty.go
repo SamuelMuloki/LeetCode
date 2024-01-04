05bb4c2aca643b3437f85f35af0b0416a6b823a3
Samuel Muloki <samuellebron0@gmail.com>
2024-01-04 10:45:35 +0300

    [Update] Use math ceil

--- go/solutions/2870-minimum-number-of-operations-to-make-array-empty.go
+++ go/solutions/2870-minimum-number-of-operations-to-make-array-empty.go
@@ -1,26 +1,21 @@
 package solutions
 
+import "math"
+
 func MinEmptyOperations(nums []int) int {
 	set := make(map[int]int)
 	for i := range nums {
 		set[nums[i]]++
 	}
 
-	res := 0
+	ans := 0
 	for _, count := range set {
-		for count > 1 {
-			if count%3 == 0 {
-				count -= 3
-			} else {
-				count -= 2
-			}
-			res++
-		}
-
 		if count == 1 {
 			return -1
 		}
+
+		ans += int(math.Ceil(float64(count) / 3))
 	}
 
-	return res
+	return ans
 }
