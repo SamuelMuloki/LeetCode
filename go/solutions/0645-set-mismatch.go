aa3fb6d1c183116210982a495ae71267852d7cd3
Samuel Muloki <samuellebron0@gmail.com>
2024-01-22 11:45:12 +0300

    [Update] Use is duplicate logic

--- go/solutions/0645-set-mismatch.go
+++ go/solutions/0645-set-mismatch.go
@@ -1,19 +1,19 @@
 package solutions
 
 func FindErrorNums(nums []int) []int {
-	n := len(nums)
-	arr := make([]int, n+1)
 	var rep int
 	for i := range nums {
-		arr[nums[i]]++
-		if arr[nums[i]] == 2 {
-			rep = nums[i]
+		idx := abs(nums[i]) - 1
+		if nums[idx] > 0 {
+			nums[idx] = -nums[idx]
+		} else {
+			rep = abs(nums[i])
 		}
 	}
 
-	for i := 1; i <= n; i++ {
-		if arr[i] == 0 {
-			return []int{rep, i}
+	for i := range nums {
+		if nums[i] > 0 {
+			return []int{rep, i + 1}
 		}
 	}
 
