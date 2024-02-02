b1f3c188130d0bb341807462cc80d1571f58df02
Samuel Muloki <samuellebron0@gmail.com>
2024-02-02 11:27:39 +0300

    [Update] Add more efficient solution

--- go/solutions/1291-sequential-digits.go
+++ go/solutions/1291-sequential-digits.go
@@ -1,29 +1,19 @@
 package solutions
 
+import "sort"
+
 func SequentialDigits(low int, high int) []int {
-	allNums := []int{
-		12, 23, 34, 45, 56, 67, 78, 89,
-		123, 234, 345, 456, 567, 678, 789,
-		1234, 2345, 3456, 4567, 5678, 6789,
-		12345, 23456, 34567, 45678, 56789,
-		123456, 234567, 345678, 456789,
-		1234567, 2345678, 3456789,
-		12345678, 23456789, 123456789,
-	}
-
 	ans := make([]int, 0)
-	n := len(allNums)
-	for i := 0; i < n; i++ {
-		if allNums[i] < low {
-			continue
+	for i := 1; i <= 9; i++ {
+		num := i
+		for j := i + 1; j <= 9; j++ {
+			num = num*10 + j
+			if num >= low && num <= high {
+				ans = append(ans, num)
+			}
 		}
-
-		if allNums[i] > high {
-			break
-		}
-
-		ans = append(ans, allNums[i])
 	}
 
+	sort.Ints(ans)
 	return ans
 }
