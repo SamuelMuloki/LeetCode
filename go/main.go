46aa38ecc24fab3626b513daec46f5ee94b672a5
Samuel Muloki <samuellebron0@gmail.com>
2024-02-16 11:04:58 +0300

    [Update] Add least number of unique ints after rems

--- go/main.go
+++ go/main.go
@@ -1221,4 +1221,5 @@ func main() {
 		{3, 1, 1}, {2, 5, 1}, {1, 5, 5}, {2, 1, 1},
 	}))
 	fmt.Printf("The largest possible perimeter of a polygon whose sides can be formed is %d\n", solutions.LargestPerimeter([]int{1, 12, 1, 2, 5, 50, 3}))
+	fmt.Printf("The Least number of unique ints after k removals is %d\n", solutions.FindLeastNumOfUniqueInts([]int{4, 3, 1, 1, 3, 3, 2}, 3))
 }
