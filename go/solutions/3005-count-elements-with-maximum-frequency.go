671b57088d1aed581b9ff8c403250c03aef73580
Samuel Muloki <samuellebron0@gmail.com>
2024-03-08 10:21:27 +0300

    [Update] Add one pass solution

--- go/solutions/3005-count-elements-with-maximum-frequency.go
+++ go/solutions/3005-count-elements-with-maximum-frequency.go
@@ -1,17 +1,15 @@
 package solutions
 
 func MaxFrequencyElements(nums []int) int {
-	freq, maxFreq := make(map[int]int), 0
+	freq, maxFreq, ans := make(map[int]int), 0, 0
 	for i := range nums {
 		freq[nums[i]]++
 
-		maxFreq = max(maxFreq, freq[nums[i]])
-	}
-
-	ans := 0
-	for _, val := range freq {
-		if val == maxFreq {
-			ans += val
+		frequency := freq[nums[i]]
+		if frequency > maxFreq {
+			maxFreq, ans = frequency, frequency
+		} else if frequency == maxFreq {
+			ans += frequency
 		}
 	}
 
