2f04acf69f7bb59efcd2030aa72660abf20f1d09
Samuel Muloki <samuellebron0@gmail.com>
2024-03-26 11:07:47 +0300

    [Update] Use cycle sort

--- go/solutions/0041-first-missing-positive.go
+++ go/solutions/0041-first-missing-positive.go
@@ -2,39 +2,19 @@ package solutions
 
 func FirstMissingPositive(nums []int) int {
 	n := len(nums)
-	contains1 := false
-
-	for i := 0; i < n; i++ {
-		if nums[i] == 1 {
-			contains1 = true
-		}
-
-		if nums[i] <= 0 || nums[i] > n {
-			nums[i] = 1
-		}
-	}
-
-	if !contains1 {
-		return 1
-	}
-
-	for i := 0; i < n; i++ {
-		val := abs(nums[i])
-		if val == n {
-			nums[0] = -abs(nums[0])
+	i := 0
+	for i < n {
+		if nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 		} else {
-			nums[val] = -abs(nums[val])
+			i++
 		}
 	}
 
-	for i := 1; i < n; i++ {
-		if nums[i] > 0 {
-			return i
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
+			return i + 1
 		}
-	}
-
-	if nums[0] > 0 {
-		return n
 	}
 
 	return n + 1
