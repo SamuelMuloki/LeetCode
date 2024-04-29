1e4fb74c26f77dd202969bfb6aed7d041880533e
Samuel Muloki <samuellebron0@gmail.com>
2024-04-29 10:13:05 +0300

    [New] Add minimum num of ops to make x array eq k

---
+++ go/solutions/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.go
@@ -0,0 +1,20 @@
+package solutions
+
+func MinOperations4(nums []int, k int) int {
+	finalXor := 0
+	for i := range nums {
+		finalXor ^= nums[i]
+	}
+
+	count := 0
+	for k > 0 || finalXor > 0 {
+		if k%2 != finalXor%2 {
+			count++
+		}
+
+		k /= 2
+		finalXor /= 2
+	}
+
+	return count
+}
