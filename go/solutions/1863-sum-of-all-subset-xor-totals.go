cfa96230689590d6af517599d14ad666936b27f4
Samuel Muloki <samuellebron0@gmail.com>
2024-05-20 11:37:14 +0300

    [Update] Use bit manipulation

--- go/solutions/1863-sum-of-all-subset-xor-totals.go
+++ go/solutions/1863-sum-of-all-subset-xor-totals.go
@@ -2,27 +2,9 @@ package solutions
 
 func SubsetXORSum(nums []int) int {
 	ans := 0
-	curr := make([]int, 0)
-
-	var backtrack func(idx int)
-	backtrack = func(idx int) {
-		currXor := 0
-		for i := range curr {
-			currXor ^= curr[i]
-		}
-		ans += currXor
-		if idx == len(nums) {
-			return
-		}
-
-		for i := idx; i < len(nums); i++ {
-			curr = append(curr, nums[i])
-			backtrack(i + 1)
-			curr = curr[:len(curr)-1]
-		}
+	for _, num := range nums {
+		ans |= num
 	}
 
-	backtrack(0)
-
-	return ans
+	return ans << (len(nums) - 1)
 }
