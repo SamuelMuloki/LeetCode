b447d5fb4a247eb09d8b479045f91591f69ca0c5
Samuel Muloki <samuellebron0@gmail.com>
2024-06-05 11:47:02 +0300

    [Update] Add optimised solution

--- go/solutions/1002-find-common-characters.go
+++ go/solutions/1002-find-common-characters.go
@@ -1,29 +1,28 @@
 package solutions
 
 func CommonChars(words []string) []string {
-	count := make([]map[byte]int, len(words))
-	for i := range words {
-		cnt := make(map[byte]int)
-		for j := range words[i] {
-			cnt[words[i][j]]++
+	wordsSize := len(words)
+	var currentCharacterCounts [26]int
+	commonCharacterCounts := [26]int{}
+	for _, ch := range words[0] {
+		commonCharacterCounts[ch-'a']++
+	}
+
+	for i := 1; i < wordsSize; i++ {
+		currentCharacterCounts = [26]int{}
+		for _, ch := range words[i] {
+			currentCharacterCounts[ch-'a']++
 		}
-		count[i] = cnt
+
+		for letter := 0; letter < 26; letter++ {
+			commonCharacterCounts[letter] = min(commonCharacterCounts[letter], currentCharacterCounts[letter])
+		}
 	}
 
 	ans := make([]string, 0)
-	for k, v := range count[0] {
-		cnt := v
-		for i := 1; i < len(count); i++ {
-			if val, found := count[i][k]; found {
-				cnt = min(cnt, val)
-			} else {
-				cnt = 0
-				break
-			}
-		}
-
-		for i := 0; i < cnt; i++ {
-			ans = append(ans, string(k))
+	for letter := 0; letter < 26; letter++ {
+		for commonCount := 0; commonCount < commonCharacterCounts[letter]; commonCount++ {
+			ans = append(ans, string(byte(letter+'a')))
 		}
 	}
 
