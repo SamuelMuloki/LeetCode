ae2e54c802b1fc2610ac93b02d54c78c1786a807
Samuel Muloki <samuellebron0@gmail.com>
2024-06-18 12:03:19 +0300

    [New] Add most profit assigning work

--- go/main.go
+++ go/main.go
@@ -1346,4 +1346,5 @@ func main() {
 	fmt.Printf("The final maximized capital is %d\n", solutions.FindMaximizedCapital(2, 0, []int{1, 2, 3}, []int{0, 1, 1}))
 	fmt.Printf("The minimum number of patches required are %d\n", solutions.MinPatches([]int{1, 3}, 6))
 	fmt.Printf("There're two integers a and b such that a2 + b2 = c: %t\n", solutions.JudgeSquareSum(5))
+	fmt.Printf("The maximum profit we can achieve after assigning the workers to the jobs is %d\n", solutions.MaxProfitAssignment([]int{2, 4, 6, 8, 10}, []int{10, 20, 30, 40, 50}, []int{4, 5, 6, 7}))
 }
