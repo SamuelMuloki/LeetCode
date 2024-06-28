3131350ded8f22f0058750b3c31ee81f938e5db0
Samuel Muloki <samuellebron0@gmail.com>
2024-06-28 11:27:04 +0300

    [Update] Add optimised solution

--- go/solutions/2285-maximum-total-importance-of-roads.go
+++ go/solutions/2285-maximum-total-importance-of-roads.go
@@ -3,31 +3,18 @@ package solutions
 import "sort"
 
 func MaximumImportance(n int, roads [][]int) int64 {
-	connections := make(map[int]int)
-	for i := range roads {
-		connections[roads[i][0]]++
-		connections[roads[i][1]]++
+	degree := make([]int, n)
+	for _, edge := range roads {
+		degree[edge[0]]++
+		degree[edge[1]]++
 	}
 
-	keys := make([]int, 0)
-	for k := range connections {
-		keys = append(keys, k)
+	sort.Ints(degree)
+	value, ans := int64(1), int64(0)
+	for _, d := range degree {
+		ans += (value * int64(d))
+		value++
 	}
 
-	sort.SliceStable(keys, func(i, j int) bool {
-		return connections[keys[i]] < connections[keys[j]]
-	})
-
-	assign, N := make(map[int]int), len(keys)
-	start := n - N + 1
-	for i := range keys {
-		assign[keys[i]] = start + i
-	}
-
-	ans := 0
-	for i := range roads {
-		ans += assign[roads[i][0]] + assign[roads[i][1]]
-	}
-
-	return int64(ans)
+	return ans
 }
