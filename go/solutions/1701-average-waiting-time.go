ea82b578ec3b452a81509e3ca017b1b0490cd206
Samuel Muloki <samuellebron0@gmail.com>
2024-07-09 10:20:32 +0300

    [Update] Refactor solution

--- go/solutions/1701-average-waiting-time.go
+++ go/solutions/1701-average-waiting-time.go
@@ -1,16 +1,11 @@
 package solutions
 
 func AverageWaitingTime(customers [][]int) float64 {
-	n := len(customers)
-	lastTime, sum := 0, 0
-	for i := 0; i < n; i++ {
-		curr := customers[i][0] + customers[i][1]
-		if lastTime > customers[i][0] {
-			curr = lastTime + customers[i][1]
-		}
-		sum += curr - customers[i][0]
-		lastTime = curr
+	nextWaitTime, nextIdleTime := 0, 0
+	for i := 0; i < len(customers); i++ {
+		nextIdleTime = max(nextIdleTime, customers[i][0]) + customers[i][1]
+		nextWaitTime += nextIdleTime - customers[i][0]
 	}
 
-	return float64(sum) / float64(n)
+	return float64(nextWaitTime) / float64(len(customers))
 }
