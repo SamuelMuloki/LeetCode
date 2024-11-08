b5ed1e00745a1abfc5b012e26776fdf738826921
samuelMuloki <samuellebron0@gmail.com>
2024-11-08 12:21:15 +0000

    [New] Add maximum xor for each query

--- go/main.go
+++ go/main.go
@@ -1544,4 +1544,5 @@ func main() {
 	fmt.Printf("The minimum number of changes required to make the string s beautiful is %d\n", solutions.MinChanges("1100010"))
 	fmt.Printf("The slice can be sorted %t\n", solutions.CanSortArray([]int{8, 4, 2, 30, 15}))
 	fmt.Printf("The largest combination with bitwise AND greater than zero is %d\n", solutions.LargestCombination([]int{16, 17, 71, 62, 12, 24, 14}))
+	fmt.Printf("The maximum xor for each query is %v\n", solutions.GetMaximumXor([]int{0, 1, 1, 3}, 2))
 }
