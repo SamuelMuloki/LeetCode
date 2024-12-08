abb0dba487c50bb28b657a5fb5a377e4162586dd
samuelMuloki <samuellebron0@gmail.com>
2024-12-08 18:20:19 +0000

    [Update] Use binary search

--- go/solutions/2054-two-best-non-overlapping-events.go
+++ go/solutions/2054-two-best-non-overlapping-events.go
@@ -3,26 +3,33 @@ package solutions
 import "sort"
 
 func MaxTwoEvents(events [][]int) int {
-	times := make([][3]int, 0)
-	for _, e := range events {
-		times = append(times, [3]int{e[0], 1, e[2]})
-		times = append(times, [3]int{e[1] + 1, 0, e[2]})
-	}
-
-	ans, maxValue := 0, 0
-	sort.Slice(times, func(i, j int) bool {
-		if times[i][0] == times[j][0] {
-			return times[i][1] < times[j][1]
-		}
-		return times[i][0] < times[j][0]
+	sort.Slice(events, func(i, j int) bool {
+		return events[i][0] < events[j][0]
 	})
 
-	for _, time := range times {
-		if time[1] == 1 {
-			ans = max(ans, time[2]+maxValue)
-		} else {
-			maxValue = max(maxValue, time[2])
+	n := len(events)
+	arr := make([]int, n+1)
+	for i := n - 1; i >= 0; i-- {
+		arr[i] = max(arr[i+1], events[i][2])
+	}
+
+	ans := 0
+	for _, event := range events {
+		maxValue := event[2]
+		left, right := 0, n
+		for left < right {
+			mid := (left + right) >> 1
+			if events[mid][0] > event[1] {
+				right = mid
+			} else {
+				left = mid + 1
+			}
 		}
+
+		if left < n {
+			maxValue += arr[left]
+		}
+		ans = max(ans, maxValue)
 	}
 
 	return ans
