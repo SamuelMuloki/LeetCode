254c194a70e9059c728537eba1eb4d1296092251
samuelMuloki <samuellebron0@gmail.com>
2024-12-31 12:56:37 +0000

    [Update] Use bottom up

--- go/solutions/0983-minimum-cost-for-tickets.go
+++ go/solutions/0983-minimum-cost-for-tickets.go
@@ -1,38 +1,24 @@
 package solutions
 
 func MincostTickets(days []int, costs []int) int {
-	isTravelDay := [366]bool{}
-	for _, day := range days {
-		isTravelDay[day] = true
-	}
-
 	lastDay := days[len(days)-1]
 	dp := make([]int, lastDay+1)
-	for i := range dp {
-		dp[i] = -1
+
+	i := 0
+	for day := 1; day <= lastDay; day++ {
+		if day < days[i] {
+			dp[day] = dp[day-1]
+		} else {
+			i++
+			dp[day] = min(
+				dp[day-1]+costs[0],
+				min(
+					dp[max(0, day-7)]+costs[1],
+					dp[max(0, day-30)]+costs[2],
+				),
+			)
+		}
 	}
 
-	var solve func(currDay int) int
-	solve = func(currDay int) int {
-		if currDay > lastDay {
-			return 0
-		}
-
-		if !isTravelDay[currDay] {
-			return solve(currDay + 1)
-		}
-
-		if dp[currDay] != -1 {
-			return dp[currDay]
-		}
-
-		oneDay := costs[0] + solve(currDay+1)
-		sevenDay := costs[1] + solve(currDay+7)
-		thirtyDay := costs[2] + solve(currDay+30)
-
-		dp[currDay] = min(oneDay, min(sevenDay, thirtyDay))
-		return dp[currDay]
-	}
-
-	return solve(1)
+	return dp[lastDay]
 }
