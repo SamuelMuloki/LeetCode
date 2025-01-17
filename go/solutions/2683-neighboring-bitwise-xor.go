f87cc2e477199072fd2a7373aff155023a78becf
samuelMuloki <samuellebron0@gmail.com>
2025-01-17 12:13:02 +0000

    [New] Add optimised solution

--- go/solutions/2683-neighboring-bitwise-xor.go
+++ go/solutions/2683-neighboring-bitwise-xor.go
@@ -1,21 +1,10 @@
 package solutions
 
 func DoesValidArrayExist(derived []int) bool {
-	n := len(derived)
-	original := make([]int, n)
-	for i := 0; i < n-1; i++ {
-		original[i+1] = derived[i] ^ original[i]
+	XOR := 0
+	for _, num := range derived {
+		XOR ^= num
 	}
 
-	checkForZero := derived[n-1] == (original[n-1] ^ original[0])
-
-	original = make([]int, n)
-	original[0] = 1
-	for i := 0; i < n-1; i++ {
-		original[i+1] = derived[i] ^ original[i]
-	}
-
-	checkForOne := derived[n-1] == (original[n-1] ^ original[0])
-
-	return checkForZero || checkForOne
+	return XOR == 0
 }
