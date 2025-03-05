bea95ad0a991b5a8efde10136211227dec6b16c3
samuelMuloki <samuellebron0@gmail.com>
2025-03-05 12:31:18 +0000

    [Update] Add one liner

--- go/solutions/2579-count-total-number-of-colored-cells.go
+++ go/solutions/2579-count-total-number-of-colored-cells.go
@@ -1,11 +1,5 @@
 package solutions
 
 func ColoredCells(n int) int64 {
-	ans, cnt := int64(1), int64(0)
-	for i := n; i > 0; i-- {
-		ans += cnt
-		cnt += 4
-	}
-
-	return ans
+	return int64(1 + n*(n-1)*2)
 }
