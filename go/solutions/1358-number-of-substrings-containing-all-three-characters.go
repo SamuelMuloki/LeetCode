ebc8d6a31cbdf42b4ae957e67361fe87e83f31d7
samuelMuloki <samuellebron0@gmail.com>
2025-03-11 12:05:04 +0000

    [Update] Use array

--- go/solutions/1358-number-of-substrings-containing-all-three-characters.go
+++ go/solutions/1358-number-of-substrings-containing-all-three-characters.go
@@ -2,20 +2,15 @@ package solutions
 
 func NumberOfSubstrings(s string) int {
 	n := len(s)
-	chSet := make(map[byte]int)
-	ans := 0
-	start, end := 0, 0
-	for end < n {
-		chSet[s[end]]++
-		for start < n && len(chSet) == 3 {
-			ans += n - end
-			chSet[s[start]]--
-			if chSet[s[start]] == 0 {
-				delete(chSet, s[start])
-			}
+	chSet := [3]int{}
+	start, ans := 0, 0
+	for end := 0; end < n; end++ {
+		chSet[s[end]-'a']++
+		for chSet[0] > 0 && chSet[1] > 0 && chSet[2] > 0 {
+			ans += (n - end)
+			chSet[s[start]-'a']--
 			start++
 		}
-		end++
 	}
 
 	return ans
