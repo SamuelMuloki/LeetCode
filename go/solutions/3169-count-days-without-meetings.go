5352769b3c2717e9ae0a2e401f06111850bc565c
samuelMuloki <samuellebron0@gmail.com>
2025-03-24 14:07:07 +0000

    [Update] Change line sweep format

--- go/solutions/3169-count-days-without-meetings.go
+++ go/solutions/3169-count-days-without-meetings.go
@@ -2,39 +2,34 @@ package solutions
 
 import "sort"
 
-type Event struct {
-	day, delta int
-}
-
 func CountDays(days int, meetings [][]int) int {
-	events := make([]Event, 0, len(meetings)*2)
-
+	dayMap := make(map[int]int)
+	prevDay := days
 	for _, meeting := range meetings {
-		events = append(events, Event{meeting[0], 1})
-		events = append(events, Event{meeting[1] + 1, -1})
+		prevDay = min(prevDay, meeting[0])
+		dayMap[meeting[0]]++
+		dayMap[meeting[1]+1]--
 	}
 
-	sort.Slice(events, func(i, j int) bool {
-		return events[i].day < events[j].day ||
-			(events[i].day == events[j].day && events[i].delta < events[j].delta)
-	})
+	keys := []int{}
+	for k := range dayMap {
+		keys = append(keys, k)
+	}
 
-	freeDays := 0
-	activeMeetings := 0
-	prevDay := 1
-	for _, event := range events {
-		currDay := event.day
-		if activeMeetings == 0 && currDay > prevDay {
+	sort.Ints(keys)
+
+	sum := 0
+	freeDays := prevDay - 1
+	for _, currDay := range keys {
+		if sum == 0 {
 			freeDays += currDay - prevDay
 		}
 
-		activeMeetings += event.delta
+		sum += dayMap[currDay]
 		prevDay = currDay
 	}
 
-	if activeMeetings == 0 && prevDay <= days {
-		freeDays += days - prevDay + 1
-	}
+	freeDays += days - prevDay + 1
 
 	return freeDays
 }
