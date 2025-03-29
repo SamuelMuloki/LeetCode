8779713e5e2d9899e155d22c15ad5f7b12eb53f3
samuelMuloki <samuellebron0@gmail.com>
2025-03-29 18:32:06 +0000

    [Update] Add more efficient solution

--- go/solutions/0503-next-greater-element-ii.go
+++ go/solutions/0503-next-greater-element-ii.go
@@ -2,25 +2,19 @@ package solutions
 
 func NextGreaterElementsII(nums []int) []int {
 	n := len(nums)
-	ng := make(map[int]int)
+	res := make([]int, n)
 	st := []int{}
-	for i := 0; i < n*2; i++ {
-		for len(st) > 0 && nums[st[len(st)-1]%n] < nums[i%n] {
-			top := st[len(st)-1]
+	for i := 2*n - 1; i > 0; i-- {
+		for len(st) > 0 && nums[st[len(st)-1]] <= nums[i%n] {
 			st = st[:len(st)-1]
-			ng[top%n] = i % n
 		}
-		st = append(st, i)
-	}
-
-	arr := make([]int, n)
-	for i := range nums {
-		if _, ok := ng[i]; ok {
-			arr[i] = nums[ng[i]]
+		if len(st) == 0 {
+			res[i%n] = -1
 		} else {
-			arr[i] = -1
+			res[i%n] = nums[st[len(st)-1]]
 		}
+		st = append(st, i%n)
 	}
 
-	return arr
+	return res
 }
