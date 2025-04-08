d152b99f6187c78a99a1bf7634416ee4827fa924
samuelMuloki <samuellebron0@gmail.com>
2025-04-08 11:28:54 +0100

    [Update] Return right aw

--- go/solutions/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.go
+++ go/solutions/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.go
@@ -2,19 +2,12 @@ package solutions
 
 func MinimumOperations3(nums []int) int {
 	cnt := [101]int{}
-	lastRepeatedChar := 0
 	for i := len(nums) - 1; i >= 0; i-- {
 		cnt[nums[i]]++
 		if cnt[nums[i]] > 1 {
-			lastRepeatedChar = i + 1
-			break
+			return i/3 + 1
 		}
 	}
 
-	div := lastRepeatedChar / 3
-	if lastRepeatedChar%3 != 0 {
-		return div + 1
-	}
-
-	return div
+	return 0
 }
