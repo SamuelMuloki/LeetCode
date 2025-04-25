6ba958ff5a2649b4d34f115516981066f606a3b3
samuelMuloki <samuellebron0@gmail.com>
2025-04-25 15:10:39 +0100

    [Update] Use a single loop

--- go/solutions/2845-count-of-interesting-subarrays.go
+++ go/solutions/2845-count-of-interesting-subarrays.go
@@ -2,23 +2,19 @@ package solutions
 
 func CountInterestingSubarrays(nums []int, modulo int, k int) int64 {
 	n := len(nums)
-	cnt := make([]int, n+1)
-	curr := 0
-	for i, num := range nums {
-		if num%modulo == k {
-			curr++
-		}
-		cnt[i+1] = curr
-	}
-
 	var res int64
+	currCount := 0
 	freq := make(map[int]int)
 	freq[0] = 1
 	for end := 0; end < n; end++ {
-		currSum := cnt[end+1] % modulo
-		target := (currSum - k + modulo) % modulo
+		if nums[end]%modulo == k {
+			currCount++
+		}
+
+		currMod := currCount % modulo
+		target := (currMod - k + modulo) % modulo
 		res += int64(freq[target])
-		freq[currSum]++
+		freq[currMod]++
 	}
 
 	return res
