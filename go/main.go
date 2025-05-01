e6f4060452bdef3df474783c266284b075a77ec1
samuelMuloki <samuellebron0@gmail.com>
2025-05-01 12:34:43 +0100

    [New] Add maximum number of tasks you can assign

--- go/main.go
+++ go/main.go
@@ -1721,4 +1721,5 @@ func main() {
 	fmt.Printf("The number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number is %d\n", solutions.CountSubarrays3([]int{1, 2, 1, 4, 1}))
 	fmt.Printf("The most common response across all days after removing duplicate responses within each responses[i] is %s\n", solutions.FindCommonResponse([][]string{{"good", "ok", "good", "ok"}, {"ok", "bad", "good", "ok", "ok"}, {"good"}, {"bad"}}))
 	fmt.Printf("The number of non-empty subarrays of nums whose score is strictly less than k is %d\n", solutions.CountSubarrays4([]int{2, 1, 4, 3, 5}, 10))
+	fmt.Printf("The maximum number of tasks that can be completed is %d\n", solutions.MaxTaskAssign([]int{3, 2, 1}, []int{0, 3, 3}, 1, 1))
 }
