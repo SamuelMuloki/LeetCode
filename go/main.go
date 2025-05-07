f2832c6d6c34137089c90157fe14d7474aee1316
samuelMuloki <samuellebron0@gmail.com>
2025-05-07 14:45:09 +0100

    [New] Add find the minimum time to reach last room I

--- go/main.go
+++ go/main.go
@@ -1727,4 +1727,5 @@ func main() {
 	fmt.Printf("The number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j] is %d\n", solutions.NumEquivDominoPairs([][]int{{1, 2}, {2, 1}, {3, 4}, {5, 6}}))
 	fmt.Printf("The number of ways to tile an 2 x n board is %d\n", solutions.NumTilings(3))
 	fmt.Printf("The minimum number of steps to walk from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1) given that you can eliminate at most k obstacles is %d\n", solutions.ShortestPath([][]int{{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}}, 1))
+	fmt.Printf("The minimum time to reach the room (n - 1, m - 1) is %d\n", solutions.MinTimeToReach([][]int{{0, 4}, {4, 4}}))
 }
