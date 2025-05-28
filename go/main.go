1e32e653a3893bdbb24276dbdad2b7d20ecc5684
samuelMuloki <samuellebron0@gmail.com>
2025-05-28 12:06:46 +0100

    [New] Add maximize number of target nodes after connecting trees

--- go/main.go
+++ go/main.go
@@ -1740,4 +1740,5 @@ func main() {
 	fmt.Printf("The maximum number of elements that can be removed from queries is %d\n", solutions.MaxRemoval([]int{2, 0, 2}, [][]int{{0, 2}, {0, 2}, {1, 1}}))
 	fmt.Printf("The length of the longest palindrome that you can create is %d\n", solutions.LongestPalindrome3([]string{"lc", "cl", "gg"}))
 	fmt.Printf("The largest color value of any valid path in the given graph is %d\n", solutions.LargestPathValue("abaca", [][]int{{0, 1}, {0, 2}, {2, 3}, {3, 4}}))
+	fmt.Printf("The maximum possible number of nodes target to node is %v\n", solutions.MaxTargetNodes([][]int{{0, 1}, {0, 2}, {2, 3}, {2, 4}}, [][]int{{0, 1}, {0, 2}, {0, 3}, {2, 7}, {1, 4}, {4, 5}, {4, 6}}, 2))
 }
