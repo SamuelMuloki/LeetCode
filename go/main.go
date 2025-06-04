903f97ea1694343c135445340655ccd79e60a405
samuelMuloki <samuellebron0@gmail.com>
2025-06-04 12:24:11 +0100

    [New] Add find the lexicographically largest string from the box

--- go/main.go
+++ go/main.go
@@ -1746,4 +1746,5 @@ func main() {
 	fmt.Printf("The least number of dice rolls required to reach the square n2. If it is not possible to reach the square is %d\n", solutions.SnakesAndLadders([][]int{{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, 35, -1, 13, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 15, -1, -1, -1, -1}}))
 	fmt.Printf("The total number of ways to distribute n candies among 3 children is %d\n", solutions.DistributeCandies(5, 2))
 	fmt.Printf("The maximum number of candies you can get following the rules is %d\n", solutions.MaxCandies([]int{1, 0, 1, 0}, []int{7, 5, 4, 100}, [][]int{{}, {}, {1}, {}}, [][]int{{1, 2}, {3}, {}, {}}, []int{0}))
+	fmt.Printf("The lexicographically largest string from the box after all the rounds are finished is %s\n", solutions.AnswerString("dbca", 2))
 }
