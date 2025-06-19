296d3bb7eaeabacf11b4f6d3b3d5c4b9b442f2ea
samuelMuloki <samuellebron0@gmail.com>
2025-06-19 12:33:29 +0100

    [New] Add partition array such that max diff is k

--- go/main.go
+++ go/main.go
@@ -1757,4 +1757,5 @@ func main() {
 	fmt.Printf("The max difference between a and b is %d\n", solutions.MaxDiff(555))
 	fmt.Printf("The maximum difference is %d\n", solutions.MaximumDifference([]int{7, 1, 5, 4}))
 	fmt.Printf("The number of good arrays that can be formed is %d\n", solutions.CountGoodArrays(3, 2, 1))
+	fmt.Printf("The minimum number of subsequences needed such that the difference between the maximum and minimum values in each subsequence is at most k is %d\n", solutions.PartitionArray([]int{3, 6, 1, 2, 5}, 2))
 }
