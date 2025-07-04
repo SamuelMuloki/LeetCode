99ed8bcb500f0b4638638deb286a61391050a5f5
samuelMuloki <samuellebron0@gmail.com>
2025-07-04 12:56:33 +0100

    [New] Add find the kth character in string game II

--- go/main.go
+++ go/main.go
@@ -1772,4 +1772,5 @@ func main() {
 	fmt.Printf("The total number of possible original strings that Alice might have intended to type is %d\n", solutions.PossibleStringCount("abbcccc"))
 	fmt.Printf("The total number of possible original strings that Alice might have intended to type, if she was trying to type a string of size at least k is %d\n", solutions.PossibleStringCount2("aabbccdd", 7))
 	fmt.Printf("The value of the kth character in word is %c\n", solutions.KthCharacter(5))
+	fmt.Printf("The value of the kth character in word after performing all the operations is %c\n", solutions.KthCharacter2(5, []int{0, 0, 0}))
 }
