148c454c9f63101b3dd0950aea2fa6a847b4e040
samuelMuloki <samuellebron0@gmail.com>
2025-07-10 11:49:08 +0100

    [New] Add reschedule meetings for max free time II

--- go/main.go
+++ go/main.go
@@ -1779,4 +1779,5 @@ func main() {
 	fmt.Printf("The maximum number of events you can attend is %d\n", solutions.MaxEvents([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 2}}))
 	fmt.Printf("The maximum sum of values that you can receive by attending events is %d\n", solutions.MaxValue([][]int{{1, 2, 4}, {3, 4, 3}, {2, 3, 10}}, 2))
 	fmt.Printf("The maximum amount of free time possible after rearranging the meetings is %d\n", solutions.MaxFreeTime(5, 1, []int{1, 3}, []int{2, 5}))
+	fmt.Printf("The maximum amount of free time II possible after rearranging the meetings is %d\n", solutions.MaxFreeTime2(5, []int{1, 3}, []int{2, 5}))
 }
