9c597fb61f1d9cee894afc3b3c090211007e618b
samuelMuloki <samuellebron0@gmail.com>
2025-08-30 16:24:27 +0100

    [New] Add valid sudoku

--- go/main.go
+++ go/main.go
@@ -1813,4 +1813,15 @@ func main() {
 	fmt.Printf("The length of the longest V-shaped diagonal segment is %d\n", solutions.LenOfVDiagonal([][]int{{2, 2, 1, 2, 2}, {2, 0, 2, 2, 0}, {2, 0, 1, 1, 0}, {1, 0, 2, 2, 2}, {2, 0, 0, 2, 2}}))
 	fmt.Printf("The sorted matrix is %v\n", solutions.SortMatrix([][]int{{1, 7, 3}, {9, 8, 2}, {4, 5, 6}}))
 	fmt.Printf("The number of possible pairs (x, y) that satisfy the conditions is %d\n", solutions.FlowerGame(3, 2))
+	fmt.Printf("The 9*9 grid is a valid sudoku: %t\n", solutions.IsValidSudoku([][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '6', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	}))
 }
