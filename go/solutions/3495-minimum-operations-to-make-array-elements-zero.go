af3793b1893fc7c9f712563bf8cdeea76493a994
samuelMuloki <samuellebron0@gmail.com>
2025-09-06 10:11:40 +0100

    [New] Add minimum operations to make array elements zero

---
+++ go/solutions/3495-minimum-operations-to-make-array-elements-zero.go
@@ -0,0 +1,29 @@
+package solutions
+
+func MinOperations10(queries [][]int) int64 {
+	var get = func(num int) int64 {
+		var cnt int64
+		i := 1
+		base := 1
+
+		for base <= num {
+			end := base*2 - 1
+			if end > num {
+				end = num
+			}
+			cnt += int64((i+1)/2) * int64(end-base+1)
+			i++
+			base *= 2
+		}
+		return cnt
+	}
+
+	var res int64
+	for _, q := range queries {
+		count1 := get(q[1])
+		count2 := get(q[0] - 1)
+		res += (count1 - count2 + 1) / 2
+	}
+
+	return res
+}
