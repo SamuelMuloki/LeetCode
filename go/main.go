aa05c2903f74ef70856923065702103a5d48c8a7
samuelMuloki <samuellebron0@gmail.com>
2025-09-29 11:36:53 +0100

    [New] Add minimum score of triangulation of polygon

--- go/main.go
+++ go/main.go
@@ -1879,4 +1879,5 @@ func main() {
 	fmt.Printf("The number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle is %d\n", solutions.TriangleNumber([]int{2, 2, 3, 4}))
 	fmt.Printf("The area of the largest triangle that can be formed by any three different points is %.6f\n", solutions.LargestTriangleArea([][]int{{0, 0}, {0, 1}, {1, 0}, {0, 2}, {2, 0}}))
 	fmt.Printf("The largest perimeter of a triangle with a non-zero area, formed from three of these lengths is %d\n", solutions.LargestPerimeter2([]int{2, 1, 2}))
+	fmt.Printf("The minimum possible score that you can achieve with some triangulation of the polygon is %d\n", solutions.MinScoreTriangulation([]int{1, 2, 3}))
 }
