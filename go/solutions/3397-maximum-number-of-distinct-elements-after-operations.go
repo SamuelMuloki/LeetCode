df6a4714690965e62175e3fa9fde7818fb26f188
samuelMuloki <samuellebron0@gmail.com>
2025-10-18 13:17:29 +0100

    [Update] Just sort nums

--- go/solutions/3397-maximum-number-of-distinct-elements-after-operations.go
+++ go/solutions/3397-maximum-number-of-distinct-elements-after-operations.go
@@ -5,29 +5,12 @@ import (
 )
 
 func MaxDistinctElements(nums []int, k int) int {
-	n := len(nums)
-	intervals := make([][2]int64, 0, n)
-	kk := int64(k)
-	for _, x := range nums {
-		xi := int64(x)
-		intervals = append(intervals, [2]int64{
-			xi - kk, xi + kk,
-		})
-	}
-
-	sort.Slice(intervals, func(i, j int) bool {
-		if intervals[i][1] != intervals[j][1] {
-			return intervals[i][1] < intervals[j][1]
-		}
-
-		return intervals[i][0] < intervals[j][0]
-	})
-
-	curr := int64(-1 << 60)
+	sort.Ints(nums)
+	curr := -1 << 31
 	res := 0
-	for _, interval := range intervals {
-		l, r := interval[0], interval[1]
-		var candidate int64 = max(curr+1, l)
+	for _, num := range nums {
+		l, r := num-k, num+k
+		candidate := max(curr+1, l)
 		if candidate <= r {
 			curr = candidate
 			res++
