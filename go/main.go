7a6e0b505a687e4c74e30b4298cc9bab32c5bca6
samuelMuloki <samuellebron0@gmail.com>
2025-11-13 10:23:02 +0000

    [New] Add max number of operations to move ones to the end

--- go/main.go
+++ go/main.go
@@ -1923,4 +1923,5 @@ func main() {
 	fmt.Printf("The minimum number of operations required to make all elements in the array 0 is %d\n", solutions.MinOperations11([]int{0, 2}))
 	fmt.Printf("The size of the largest subset of strs such that there are at most m 0's and n 1's in the subset is %d\n", solutions.FindMaxForm([]string{"10", "0001", "111001", "1", "0"}, 5, 3))
 	fmt.Printf("The minimum number of operations required to make all elements in the array 1 is %d\n", solutions.MinOperations12([]int{2, 6, 3, 4}))
+	fmt.Printf("The maximum number of operations that you can perform are %d\n", solutions.MaxOperations2("1001101"))
 }
