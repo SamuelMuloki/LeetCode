1e56642decba5383c3fa84f1fea3ea6a33a952a2
samuelMuloki <samuellebron0@gmail.com>
2025-11-18 23:08:31 +0000

    [New] Add 1-bit and 2-bit characters

--- go/main.go
+++ go/main.go
@@ -1927,4 +1927,5 @@ func main() {
 	fmt.Printf("The matrix mat after performing every query is %v\n", solutions.RangeAddQueries(3, [][]int{{1, 1, 2, 2}, {0, 0, 1, 1}}))
 	fmt.Printf("The number of substrings with dominant ones is %d\n", solutions.NumberOfSubstrings2("00011"))
 	fmt.Printf("All 1's are at least k places away from each other %t\n", solutions.KLengthApart([]int{1, 0, 0, 0, 1, 0, 0, 1}, 2))
+	fmt.Printf("The last character must be a one-bit character is %t\n", solutions.IsOneBitCharacter([]int{1, 0, 0}))
 }
