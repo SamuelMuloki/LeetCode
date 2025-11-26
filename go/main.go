297be8f59eeaa415297641fdbb84bb093ce78aef
samuelMuloki <samuellebron0@gmail.com>
2025-11-26 09:22:11 +0000

    [New] Add function to calculate the number of paths with sum divisible by k

--- go/main.go
+++ go/main.go
@@ -1934,4 +1934,5 @@ func main() {
 	fmt.Printf("The maximum possible sum of elements of the array such that it is divisible by three is %d\n", solutions.MaxSumDivThree([]int{1, 2, 3, 4, 4}))
 	fmt.Printf("The array of booleans answer where answer[i] is true if xi is divisible by 5 is %v\n", solutions.PrefixesDivBy5([]int{0, 1, 1}))
 	fmt.Printf("The length of n. If there is no such n is %d\n", solutions.SmallestRepunitDivByK(1))
+	fmt.Printf("The number of paths where the sum of the elements on the path is divisible by k is %d\n", solutions.NumberOfPaths([][]int{{524}, {3, 0, 5}, {0, 7, 2}}, 3))
 }
