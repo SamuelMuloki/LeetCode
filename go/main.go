be8f4a32887aa3b0bb5513804801d1db5186f7ce
samuelMuloki <samuellebron0@gmail.com>
2025-12-18 09:26:02 +0000

    Add function to calculate maximum profit using a trading strategy

--- go/main.go
+++ go/main.go
@@ -1954,4 +1954,5 @@ func main() {
 	fmt.Printf("The number of smooth descent periods are %d\n", solutions.GetDescentPeriods([]int{3, 2, 1, 4}))
 	fmt.Printf("The maximum profit that can be achieved without exceeding the given budget is %d\n", solutions.MaxProfit3(2, []int{1, 2}, []int{4, 3}, [][]int{{1, 2}}, 3))
 	fmt.Printf("The maximum total profit you can earn by making at most k transactions is %d\n", solutions.MaximumProfit([]int{1, 7, 9, 8, 2}, 2))
+	fmt.Printf("The maximum possible profit you can achieve is %d\n", solutions.MaxProfit4([]int{5, 4, 3}, []int{1, 1, 0}, 2))
 }
